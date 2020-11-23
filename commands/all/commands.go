72ecb3716cdacd57831b2393eeaddf0189dbad70
Jebbs <qjebbs@gmail.com>
2020-11-23 23:38:43 +0800

    go style commands, merge v2ctl commands, v5 oriented

---
+++ commands/all/commands.go
@@ -0,0 +1,17 @@
+package all
+
+import "github.com/v2fly/v2ray-core/v4/commands/base"
+
+// go:generate go run github.com/v2fly/v2ray-core/v4/common/errors/errorgen
+
+func init() {
+	base.RootCommand.Commands = append(
+		base.RootCommand.Commands,
+		cmdAPI,
+		cmdConvert,
+		cmdLove,
+		cmdTLS,
+		cmdUUID,
+		cmdVerify,
+	)
+}
