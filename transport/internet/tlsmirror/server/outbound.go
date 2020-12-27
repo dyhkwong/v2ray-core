c277e212e7a6d9d1ef68a7b755b8e5f702e5cef9
RPRX <63339210+rprx@users.noreply.github.com>
2020-12-28 03:20:39 +0800

    Endpoint independent mapping support

--- transport/internet/tlsmirror/server/outbound.go
+++ transport/internet/tlsmirror/server/outbound.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v4/common/signal/done"
 	"github.com/v2fly/v2ray-core/v4/transport"
 )
@@ -104,7 +105,7 @@ func (co *Outbound) Dispatch(ctx context.Context, link *transport.Link) {
 	}
 
 	closeSignal := done.New()
-	c := net.NewConnection(net.ConnectionInputMulti(link.Writer), net.ConnectionOutputMulti(link.Reader), net.ConnectionOnClose(closeSignal))
+	c := cnc.NewConnection(cnc.ConnectionInputMulti(link.Writer), cnc.ConnectionOutputMulti(link.Reader), cnc.ConnectionOnClose(closeSignal))
 	co.listener.addWithContext(ctx, c)
 	co.access.RUnlock()
 	<-closeSignal.Wait()
