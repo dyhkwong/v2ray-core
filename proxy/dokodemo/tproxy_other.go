fea4be3873c62a5b22741a81ddec4688af71e60e
RPRX <63339210+rprx@users.noreply.github.com>
2020-12-31 15:57:15 +0000

    Refactor TPROXY inbound UDP writeback

---
+++ proxy/dokodemo/tproxy_other.go
@@ -0,0 +1,12 @@
+//go:build !linux
+
+package dokodemo
+
+import (
+	"fmt"
+	"net"
+)
+
+func DialUDP(addr *net.UDPAddr, mark int) (net.PacketConn, error) {
+	return nil, &net.OpError{Op: "tproxy", Err: fmt.Errorf("!linux")}
+}
