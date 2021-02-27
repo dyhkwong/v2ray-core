58352ec7d04439dc979559fe664bf467a2137dd0
monsoon <yjh1021317464@gmail.com>
2021-02-28 00:10:26 +0800

    Add HTTP+SOCKS mixed inbound

---
+++ proxy/mixed/simplified/config.pb.go
@@ -0,0 +1,144 @@
+package simplified
+
+import (
+	net "github.com/v2fly/v2ray-core/v5/common/net"
+	packetaddr "github.com/v2fly/v2ray-core/v5/common/net/packetaddr"
+	_ "github.com/v2fly/v2ray-core/v5/common/protoext"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+type ServerConfig struct {
+	state          protoimpl.MessageState    `protogen:"open.v1"`
+	Address        *net.IPOrDomain           `protobuf:"bytes,3,opt,name=address,proto3" json:"address,omitempty"`
+	UdpEnabled     bool                      `protobuf:"varint,4,opt,name=udp_enabled,json=udpEnabled,proto3" json:"udp_enabled,omitempty"`
+	PacketEncoding packetaddr.PacketAddrType `protobuf:"varint,7,opt,name=packet_encoding,json=packetEncoding,proto3,enum=v2ray.core.net.packetaddr.PacketAddrType" json:"packet_encoding,omitempty"`
+	unknownFields  protoimpl.UnknownFields
+	sizeCache      protoimpl.SizeCache
+}
+
+func (x *ServerConfig) Reset() {
+	*x = ServerConfig{}
+	mi := &file_proxy_mixed_simplified_config_proto_msgTypes[0]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ServerConfig) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ServerConfig) ProtoMessage() {}
+
+func (x *ServerConfig) ProtoReflect() protoreflect.Message {
+	mi := &file_proxy_mixed_simplified_config_proto_msgTypes[0]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ServerConfig.ProtoReflect.Descriptor instead.
+func (*ServerConfig) Descriptor() ([]byte, []int) {
+	return file_proxy_mixed_simplified_config_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *ServerConfig) GetAddress() *net.IPOrDomain {
+	if x != nil {
+		return x.Address
+	}
+	return nil
+}
+
+func (x *ServerConfig) GetUdpEnabled() bool {
+	if x != nil {
+		return x.UdpEnabled
+	}
+	return false
+}
+
+func (x *ServerConfig) GetPacketEncoding() packetaddr.PacketAddrType {
+	if x != nil {
+		return x.PacketEncoding
+	}
+	return packetaddr.PacketAddrType(0)
+}
+
+var File_proxy_mixed_simplified_config_proto protoreflect.FileDescriptor
+
+const file_proxy_mixed_simplified_config_proto_rawDesc = "" +
+	"\n" +
+	"#proxy/mixed/simplified/config.proto\x12!v2ray.core.proxy.mixed.simplified\x1a common/protoext/extensions.proto\x1a\x18common/net/address.proto\x1a\"common/net/packetaddr/config.proto\"\xd6\x01\n" +
+	"\fServerConfig\x12;\n" +
+	"\aaddress\x18\x03 \x01(\v2!.v2ray.core.common.net.IPOrDomainR\aaddress\x12\x1f\n" +
+	"\vudp_enabled\x18\x04 \x01(\bR\n" +
+	"udpEnabled\x12R\n" +
+	"\x0fpacket_encoding\x18\a \x01(\x0e2).v2ray.core.net.packetaddr.PacketAddrTypeR\x0epacketEncoding:\x14\x82\xb5\x18\x10\n" +
+	"\ainbound\x12\x05mixedB\x84\x01\n" +
+	"%com.v2ray.core.proxy.mixed.simplifiedP\x01Z5github.com/v2fly/v2ray-core/v5/proxy/mixed/simplified\xaa\x02!V2Ray.Core.Proxy.Mixed.Simplifiedb\x06proto3"
+
+var (
+	file_proxy_mixed_simplified_config_proto_rawDescOnce sync.Once
+	file_proxy_mixed_simplified_config_proto_rawDescData []byte
+)
+
+func file_proxy_mixed_simplified_config_proto_rawDescGZIP() []byte {
+	file_proxy_mixed_simplified_config_proto_rawDescOnce.Do(func() {
+		file_proxy_mixed_simplified_config_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_proxy_mixed_simplified_config_proto_rawDesc), len(file_proxy_mixed_simplified_config_proto_rawDesc)))
+	})
+	return file_proxy_mixed_simplified_config_proto_rawDescData
+}
+
+var file_proxy_mixed_simplified_config_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_proxy_mixed_simplified_config_proto_goTypes = []any{
+	(*ServerConfig)(nil),           // 0: v2ray.core.proxy.mixed.simplified.ServerConfig
+	(*net.IPOrDomain)(nil),         // 1: v2ray.core.common.net.IPOrDomain
+	(packetaddr.PacketAddrType)(0), // 2: v2ray.core.net.packetaddr.PacketAddrType
+}
+var file_proxy_mixed_simplified_config_proto_depIdxs = []int32{
+	1, // 0: v2ray.core.proxy.mixed.simplified.ServerConfig.address:type_name -> v2ray.core.common.net.IPOrDomain
+	2, // 1: v2ray.core.proxy.mixed.simplified.ServerConfig.packet_encoding:type_name -> v2ray.core.net.packetaddr.PacketAddrType
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
+}
+
+func init() { file_proxy_mixed_simplified_config_proto_init() }
+func file_proxy_mixed_simplified_config_proto_init() {
+	if File_proxy_mixed_simplified_config_proto != nil {
+		return
+	}
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_proxy_mixed_simplified_config_proto_rawDesc), len(file_proxy_mixed_simplified_config_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   1,
+			NumExtensions: 0,
+			NumServices:   0,
+		},
+		GoTypes:           file_proxy_mixed_simplified_config_proto_goTypes,
+		DependencyIndexes: file_proxy_mixed_simplified_config_proto_depIdxs,
+		MessageInfos:      file_proxy_mixed_simplified_config_proto_msgTypes,
+	}.Build()
+	File_proxy_mixed_simplified_config_proto = out.File
+	file_proxy_mixed_simplified_config_proto_goTypes = nil
+	file_proxy_mixed_simplified_config_proto_depIdxs = nil
+}
