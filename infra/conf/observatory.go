ba63cf6099835c230127ce1a8ad56b32b3fd2120
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2021-08-08 14:49:32 +0800

    Fix JSON key `probeInterval` misspelled

--- infra/conf/observatory.go
+++ infra/conf/observatory.go
@@ -10,9 +10,15 @@ import (
 type ObservatoryConfig struct {
 	SubjectSelector []string          `json:"subjectSelector"`
 	ProbeURL        string            `json:"probeURL"`
-	ProbeInterval   duration.Duration `json:"ProbeInterval"`
+	ProbeInterval   duration.Duration `json:"probeInterval"`
+	ProbeInterval2  duration.Duration `json:"ProbeInterval"` // The key was misspelled. For backward compatibility, we have to keep track of the old key.
 }
 
 func (o *ObservatoryConfig) Build() (proto.Message, error) {
-	return &observatory.Config{SubjectSelector: o.SubjectSelector, ProbeUrl: o.ProbeURL, ProbeInterval: int64(o.ProbeInterval)}, nil
+	probeInterval := int64(o.ProbeInterval)
+	probeInterval2 := int64(o.ProbeInterval2)
+	if probeInterval == 0 && probeInterval2 != 0 {
+		probeInterval = probeInterval2
+	}
+	return &observatory.Config{SubjectSelector: o.SubjectSelector, ProbeUrl: o.ProbeURL, ProbeInterval: probeInterval}, nil
 }
