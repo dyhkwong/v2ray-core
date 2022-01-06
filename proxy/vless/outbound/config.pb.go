a233187d12fdaa80c04caaf4a871d2c5591a549d
世界 <i@sekai.icu>
2022-01-06 22:56:05 +0800

    VMess, VLESS and Mux: packetaddr/xudp support

--- proxy/vless/outbound/config.pb.go
+++ proxy/vless/outbound/config.pb.go
@@ -1,6 +1,7 @@
 package outbound
 
 import (
+	packetaddr "github.com/v2fly/v2ray-core/v4/common/net/packetaddr"
 	protocol "github.com/v2fly/v2ray-core/v4/common/protocol"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
@@ -17,10 +18,11 @@ const (
 )
 
 type Config struct {
-	state         protoimpl.MessageState     `protogen:"open.v1"`
-	Vnext         []*protocol.ServerEndpoint `protobuf:"bytes,1,rep,name=vnext,proto3" json:"vnext,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	state          protoimpl.MessageState     `protogen:"open.v1"`
+	Vnext          []*protocol.ServerEndpoint `protobuf:"bytes,1,rep,name=vnext,proto3" json:"vnext,omitempty"`
+	PacketEncoding packetaddr.PacketAddrType  `protobuf:"varint,2,opt,name=packet_encoding,json=packetEncoding,proto3,enum=v2ray.core.net.packetaddr.PacketAddrType" json:"packet_encoding,omitempty"`
+	unknownFields  protoimpl.UnknownFields
+	sizeCache      protoimpl.SizeCache
 }
 
 func (x *Config) Reset() {
@@ -60,13 +62,21 @@ func (x *Config) GetVnext() []*protocol.ServerEndpoint {
 	return nil
 }
 
+func (x *Config) GetPacketEncoding() packetaddr.PacketAddrType {
+	if x != nil {
+		return x.PacketEncoding
+	}
+	return packetaddr.PacketAddrType(0)
+}
+
 var File_proxy_vless_outbound_config_proto protoreflect.FileDescriptor
 
 const file_proxy_vless_outbound_config_proto_rawDesc = "" +
 	"\n" +
-	"!proxy/vless/outbound/config.proto\x12\x1fv2ray.core.proxy.vless.outbound\x1a!common/protocol/server_spec.proto\"J\n" +
+	"!proxy/vless/outbound/config.proto\x12\x1fv2ray.core.proxy.vless.outbound\x1a!common/protocol/server_spec.proto\x1a\"common/net/packetaddr/config.proto\"\x9e\x01\n" +
 	"\x06Config\x12@\n" +
-	"\x05vnext\x18\x01 \x03(\v2*.v2ray.core.common.protocol.ServerEndpointR\x05vnextB~\n" +
+	"\x05vnext\x18\x01 \x03(\v2*.v2ray.core.common.protocol.ServerEndpointR\x05vnext\x12R\n" +
+	"\x0fpacket_encoding\x18\x02 \x01(\x0e2).v2ray.core.net.packetaddr.PacketAddrTypeR\x0epacketEncodingB~\n" +
 	"#com.v2ray.core.proxy.vless.outboundP\x01Z3github.com/v2fly/v2ray-core/v4/proxy/vless/outbound\xaa\x02\x1fV2Ray.Core.Proxy.Vless.Outboundb\x06proto3"
 
 var (
@@ -85,14 +95,16 @@ var file_proxy_vless_outbound_config_proto_msgTypes = make([]protoimpl.MessageIn
 var file_proxy_vless_outbound_config_proto_goTypes = []any{
 	(*Config)(nil),                  // 0: v2ray.core.proxy.vless.outbound.Config
 	(*protocol.ServerEndpoint)(nil), // 1: v2ray.core.common.protocol.ServerEndpoint
+	(packetaddr.PacketAddrType)(0),  // 2: v2ray.core.net.packetaddr.PacketAddrType
 }
 var file_proxy_vless_outbound_config_proto_depIdxs = []int32{
 	1, // 0: v2ray.core.proxy.vless.outbound.Config.vnext:type_name -> v2ray.core.common.protocol.ServerEndpoint
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	2, // 1: v2ray.core.proxy.vless.outbound.Config.packet_encoding:type_name -> v2ray.core.net.packetaddr.PacketAddrType
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_proxy_vless_outbound_config_proto_init() }
