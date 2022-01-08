a022250c4603e370c644eef4ccf627f1b8b84c5d
世界 <i@sekai.icu>
2022-01-08 16:30:44 +0800

    Add Shadowsocks SIP003 plugin support

--- proxy/shadowsocks2022/config.pb.go
+++ proxy/shadowsocks2022/config.pb.go
@@ -18,14 +18,18 @@ const (
 )
 
 type ClientConfig struct {
-	state         protoimpl.MessageState `protogen:"open.v1"`
-	Method        string                 `protobuf:"bytes,1,opt,name=method,proto3" json:"method,omitempty"`
-	Psk           []byte                 `protobuf:"bytes,2,opt,name=psk,proto3" json:"psk,omitempty"`
-	Ipsk          [][]byte               `protobuf:"bytes,4,rep,name=ipsk,proto3" json:"ipsk,omitempty"`
-	Address       *net.IPOrDomain        `protobuf:"bytes,5,opt,name=address,proto3" json:"address,omitempty"`
-	Port          uint32                 `protobuf:"varint,6,opt,name=port,proto3" json:"port,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	state            protoimpl.MessageState `protogen:"open.v1"`
+	Method           string                 `protobuf:"bytes,1,opt,name=method,proto3" json:"method,omitempty"`
+	Psk              []byte                 `protobuf:"bytes,2,opt,name=psk,proto3" json:"psk,omitempty"`
+	Ipsk             [][]byte               `protobuf:"bytes,4,rep,name=ipsk,proto3" json:"ipsk,omitempty"`
+	Address          *net.IPOrDomain        `protobuf:"bytes,5,opt,name=address,proto3" json:"address,omitempty"`
+	Port             uint32                 `protobuf:"varint,6,opt,name=port,proto3" json:"port,omitempty"`
+	Plugin           string                 `protobuf:"bytes,7,opt,name=plugin,proto3" json:"plugin,omitempty"`
+	PluginOpts       string                 `protobuf:"bytes,8,opt,name=plugin_opts,json=pluginOpts,proto3" json:"plugin_opts,omitempty"`
+	PluginArgs       []string               `protobuf:"bytes,9,rep,name=plugin_args,json=pluginArgs,proto3" json:"plugin_args,omitempty"`
+	PluginWorkingDir string                 `protobuf:"bytes,10,opt,name=plugin_working_dir,json=pluginWorkingDir,proto3" json:"plugin_working_dir,omitempty"`
+	unknownFields    protoimpl.UnknownFields
+	sizeCache        protoimpl.SizeCache
 }
 
 func (x *ClientConfig) Reset() {
@@ -93,17 +97,52 @@ func (x *ClientConfig) GetPort() uint32 {
 	return 0
 }
 
+func (x *ClientConfig) GetPlugin() string {
+	if x != nil {
+		return x.Plugin
+	}
+	return ""
+}
+
+func (x *ClientConfig) GetPluginOpts() string {
+	if x != nil {
+		return x.PluginOpts
+	}
+	return ""
+}
+
+func (x *ClientConfig) GetPluginArgs() []string {
+	if x != nil {
+		return x.PluginArgs
+	}
+	return nil
+}
+
+func (x *ClientConfig) GetPluginWorkingDir() string {
+	if x != nil {
+		return x.PluginWorkingDir
+	}
+	return ""
+}
+
 var File_proxy_shadowsocks2022_config_proto protoreflect.FileDescriptor
 
 const file_proxy_shadowsocks2022_config_proto_rawDesc = "" +
 	"\n" +
-	"\"proxy/shadowsocks2022/config.proto\x12 v2ray.core.proxy.shadowsocks2022\x1a\x18common/net/address.proto\x1a common/protoext/extensions.proto\"\xc2\x01\n" +
+	"\"proxy/shadowsocks2022/config.proto\x12 v2ray.core.proxy.shadowsocks2022\x1a\x18common/net/address.proto\x1a common/protoext/extensions.proto\"\xca\x02\n" +
 	"\fClientConfig\x12\x16\n" +
 	"\x06method\x18\x01 \x01(\tR\x06method\x12\x10\n" +
 	"\x03psk\x18\x02 \x01(\fR\x03psk\x12\x12\n" +
 	"\x04ipsk\x18\x04 \x03(\fR\x04ipsk\x12;\n" +
 	"\aaddress\x18\x05 \x01(\v2!.v2ray.core.common.net.IPOrDomainR\aaddress\x12\x12\n" +
-	"\x04port\x18\x06 \x01(\rR\x04port:#\x82\xb5\x18\x1f\n" +
+	"\x04port\x18\x06 \x01(\rR\x04port\x12\x16\n" +
+	"\x06plugin\x18\a \x01(\tR\x06plugin\x12\x1f\n" +
+	"\vplugin_opts\x18\b \x01(\tR\n" +
+	"pluginOpts\x12\x1f\n" +
+	"\vplugin_args\x18\t \x03(\tR\n" +
+	"pluginArgs\x12,\n" +
+	"\x12plugin_working_dir\x18\n" +
+	" \x01(\tR\x10pluginWorkingDir:#\x82\xb5\x18\x1f\n" +
 	"\boutbound\x12\x0fshadowsocks2022\x90\xff)\x01B\x81\x01\n" +
 	"$com.v2ray.core.proxy.shadowsocks2022P\x01Z4github.com/v2fly/v2ray-core/v5/proxy/shadowsocks2022\xaa\x02 V2Ray.Core.Proxy.Shadowsocks2022b\x06proto3"
 
