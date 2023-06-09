450174a56a58d71f9ccc718d3319875c6762cbc1
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-06-10 03:19:02 +0800

    Fix DNS TTL

--- features/dns/localdns/client.go
+++ features/dns/localdns/client.go
@@ -51,6 +51,18 @@ func (*Client) LookupIPv6(host string) ([]net.IP, error) {
 	return lookupFunc("ip6", host)
 }
 
+// LookupIPv4WithTTL implements IPv4LookupWithTTL.
+func (c *Client) LookupIPv4WithTTL(host string) ([]net.IP, time.Time, error) {
+	ips, err := c.LookupIPv4(host)
+	return ips, time.Now().Add(time.Duration(600) * time.Second), err
+}
+
+// LookupIPv6WithTTL implements IPv6LookupWithTTL.
+func (c *Client) LookupIPv6WithTTL(host string) ([]net.IP, time.Time, error) {
+	ips, err := c.LookupIPv6(host)
+	return ips, time.Now().Add(time.Duration(600) * time.Second), err
+}
+
 // New create a new dns.Client that queries localhost for DNS.
 func New() *Client {
 	return &Client{}
