8ea3176379098bb066c08fed21e0cc4a969d4ead
RPRX <63339210+rprx@users.noreply.github.com>
2023-09-29 21:51:24 +0800

    Import REALITY

--- transport/internet/domainsocket/listener.go
+++ transport/internet/domainsocket/listener.go
@@ -8,19 +8,23 @@ import (
 	"os"
 	"strings"
 
+	utls "github.com/metacubex/utls"
+
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/reality"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/tls"
 )
 
 type Listener struct {
-	addr      *net.UnixAddr
-	ln        net.Listener
-	tlsConfig *gotls.Config
-	config    *Config
-	addConn   internet.ConnHandler
-	locker    *fileLocker
+	addr          *net.UnixAddr
+	ln            net.Listener
+	tlsConfig     *gotls.Config
+	realityConfig *utls.RealityConfig
+	config        *Config
+	addConn       internet.ConnHandler
+	locker        *fileLocker
 }
 
 func Listen(ctx context.Context, address net.Address, port net.Port, streamSettings *internet.MemoryStreamConfig, handler internet.ConnHandler) (internet.Listener, error) {
@@ -54,6 +58,8 @@ func Listen(ctx context.Context, address net.Address, port net.Port, streamSetti
 
 	if config := tls.ConfigFromStreamSettings(streamSettings); config != nil {
 		ln.tlsConfig = config.GetTLSConfig()
+	} else if config := reality.ConfigFromStreamSettings(streamSettings); config != nil {
+		ln.realityConfig = config.GetREALITYConfig()
 	}
 
 	go ln.run()
@@ -82,12 +88,17 @@ func (ln *Listener) run() {
 			newError("failed to accepted raw connections").Base(err).AtWarning().WriteToLog()
 			continue
 		}
-
-		if ln.tlsConfig != nil {
-			conn = tls.Server(conn, ln.tlsConfig)
-		}
-
-		ln.addConn(internet.Connection(conn))
+		go func() {
+			if ln.tlsConfig != nil {
+				conn = tls.Server(conn, ln.tlsConfig)
+			} else if ln.realityConfig != nil {
+				if conn, err = utls.RealityServer(context.Background(), conn, ln.realityConfig); err != nil {
+					newError(err).AtInfo().WriteToLog()
+					return
+				}
+			}
+			ln.addConn(internet.Connection(conn))
+		}()
 	}
 }
 
