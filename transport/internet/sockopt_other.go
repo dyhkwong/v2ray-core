8844afaf5094ef3348905089e082c71635deaf1a
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-02-06 20:38:39 +0800

    Fix bindToDevice

--- transport/internet/sockopt_other.go
+++ transport/internet/sockopt_other.go
@@ -6,7 +6,7 @@ func applyOutboundSocketOptions(_ string, _ string, _ uintptr, _ *SocketConfig) 
 	return nil
 }
 
-func applyInboundSocketOptions(_ string, _ uintptr, _ *SocketConfig) error {
+func applyInboundSocketOptions(_ string, _ string, _ uintptr, _ *SocketConfig) error {
 	return nil
 }
 
