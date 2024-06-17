7e9f11bff620fa6c5d49f7c859cab02327f4f3eb
mmmray <142015632+mmmray@users.noreply.github.com>
2024-06-17 11:53:46 +0800

    Import SplitHTTP transport

--- features/extension/browser.go
+++ features/extension/browser.go
@@ -3,6 +3,8 @@ package extension
 import (
 	"io"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 type BrowserForwarder interface {
@@ -11,4 +13,14 @@ type BrowserForwarder interface {
 
 func BrowserForwarderType() interface{} {
 	return (*BrowserForwarder)(nil)
+}
+
+type BrowserDialer interface {
+	DialWS(uri string, earlydata []byte) (*websocket.Conn, error)
+	DialGet(uri string, headers http.Header) (*websocket.Conn, error)
+	DialPost(uri string, headers http.Header, payload []byte) error
+}
+
+func BrowserDialerType() interface{} {
+	return (*BrowserDialer)(nil)
 }
