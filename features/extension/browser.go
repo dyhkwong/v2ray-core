2450f02c22454ea4582b2c4ffd360eb93506ced6
mmmray <142015632+mmmray@users.noreply.github.com>
2024-06-17 11:53:46 +0800

    Import SplitHTTP transport

--- features/extension/browser.go
+++ features/extension/browser.go
@@ -3,6 +3,8 @@ package extension
 import (
 	"io"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 type BrowserForwarder interface {
@@ -11,4 +13,14 @@ type BrowserForwarder interface {
 
 func BrowserForwarderType() interface{} {
 	return (*BrowserForwarder)(nil)
+}
+
+type BrowserDialer interface {
+	DialWS(uri string, earlydata []byte) (*websocket.Conn, error)
+	DialGet(uri string, headers http.Header, cookies []*http.Cookie) (*websocket.Conn, error)
+	DialPost(method, uri string, headers http.Header, cookies []*http.Cookie, payload []byte) error
+}
+
+func BrowserDialerType() interface{} {
+	return (*BrowserDialer)(nil)
 }
---
+++ transport/internet/splithttp/hub.go
@@ -0,0 +1,459 @@
+package splithttp
+
+import (
+	"context"
+	gotls "crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"slices"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	utls "github.com/metacubex/utls"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	http_proto "github.com/v2fly/v2ray-core/v5/common/protocol/http"
+	"github.com/v2fly/v2ray-core/v5/common/session"
+	"github.com/v2fly/v2ray-core/v5/common/signal/done"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/reality"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tls"
+)
+
+type requestHandler struct {
+	config    *Config
+	host      string
+	path      string
+	ln        *Listener
+	sessionMu *sync.Mutex
+	sessions  sync.Map
+	localAddr net.Addr
+}
+
+type httpSession struct {
+	uploadQueue *uploadQueue
+	// for as long as the GET request is not opened by the client, this will be
+	// open ("undone"), and the session may be expired within a certain TTL.
+	// after the client connects, this becomes "done" and the session lives as
+	// long as the GET request.
+	isFullyConnected *done.Instance
+}
+
+func (h *requestHandler) upsertSession(sessionId string) *httpSession {
+	// fast path
+	currentSessionAny, ok := h.sessions.Load(sessionId)
+	if ok {
+		return currentSessionAny.(*httpSession)
+	}
+
+	// slow path
+	h.sessionMu.Lock()
+	defer h.sessionMu.Unlock()
+
+	currentSessionAny, ok = h.sessions.Load(sessionId)
+	if ok {
+		return currentSessionAny.(*httpSession)
+	}
+
+	s := &httpSession{
+		uploadQueue:      NewUploadQueue(h.ln.config.GetNormalizedScMaxBufferedPosts()),
+		isFullyConnected: done.New(),
+	}
+
+	h.sessions.Store(sessionId, s)
+
+	shouldReap := done.New()
+	go func() {
+		time.Sleep(30 * time.Second)
+		shouldReap.Close()
+	}()
+	go func() {
+		select {
+		case <-shouldReap.Wait():
+			h.sessions.Delete(sessionId)
+			s.uploadQueue.Close()
+		case <-s.isFullyConnected.Wait():
+		}
+	}()
+
+	return s
+}
+
+func (h *requestHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if !strings.HasPrefix(request.URL.Path, h.path) {
+		newError("failed to validate path, request:", request.URL.Path, ", config:", h.path).WriteToLog()
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	h.config.WriteResponseHeader(writer, request.Method, request.Header)
+
+	xPaddingConfig := &XPaddingConfig{
+		Length: int(h.config.GetNormalizedXPaddingBytes().rand()),
+	}
+
+	if h.config.XPaddingObfsMode {
+		xPaddingConfig.Placement = XPaddingPlacement{
+			Placement: h.config.XPaddingPlacement,
+			Key:       h.config.XPaddingKey,
+			Header:    h.config.XPaddingHeader,
+		}
+		xPaddingConfig.Method = PaddingMethod(h.config.XPaddingMethod)
+	} else {
+		xPaddingConfig.Placement = XPaddingPlacement{
+			Placement: PlacementHeader,
+			Header:    "X-Padding",
+		}
+	}
+
+	h.config.ApplyXPaddingToResponse(writer, xPaddingConfig)
+
+	if request.Method == "OPTIONS" {
+		writer.WriteHeader(http.StatusOK)
+		return
+	}
+
+	sessionId, seqStr := h.config.ExtractMetaFromRequest(request, h.path)
+
+	var remoteAddr net.Addr
+	var err error
+	remoteAddr, err = net.ResolveTCPAddr("tcp", request.RemoteAddr)
+	if err != nil {
+		remoteAddr = &net.TCPAddr{
+			IP:   net.AnyIP.IP(),
+			Port: 0,
+		}
+	}
+	if request.ProtoMajor == 3 {
+		remoteAddr = &net.UDPAddr{
+			IP:   remoteAddr.(*net.TCPAddr).IP,
+			Port: remoteAddr.(*net.TCPAddr).Port,
+		}
+	}
+	forwardedAddrs := http_proto.ParseXForwardedFor(request.Header)
+	if len(forwardedAddrs) > 0 && forwardedAddrs[0].Family().IsIP() {
+		remoteAddr = &net.TCPAddr{
+			IP:   forwardedAddrs[0].IP(),
+			Port: 0,
+		}
+	}
+
+	var currentSession *httpSession
+	if sessionId != "" {
+		currentSession = h.upsertSession(sessionId)
+	}
+
+	isUplinkRequest := false
+
+	switch request.Method {
+	case "GET":
+		isUplinkRequest = seqStr != ""
+	default:
+		isUplinkRequest = true
+	}
+
+	uplinkDataKey := h.config.UplinkDataKey
+
+	if isUplinkRequest && sessionId != "" { // stream-up, packet-up
+		if seqStr == "" {
+			httpSC := &httpServerConn{
+				Instance:       done.New(),
+				Reader:         request.Body,
+				ResponseWriter: writer,
+			}
+			err = currentSession.uploadQueue.Push(Packet{
+				Reader: httpSC,
+			})
+			if err != nil {
+				newError("failed to upload (PushReader)").Base(err).AtInfo().WriteToLog()
+				writer.WriteHeader(http.StatusConflict)
+			} else {
+				writer.Header().Set("X-Accel-Buffering", "no")
+				writer.Header().Set("Cache-Control", "no-store")
+				writer.WriteHeader(http.StatusOK)
+				select {
+				case <-request.Context().Done():
+				case <-httpSC.Wait():
+				}
+			}
+			httpSC.Close()
+			return
+		}
+
+		dataPlacement := h.config.GetNormalizedUplinkDataPlacement()
+		var headerPayload []byte
+		if dataPlacement == PlacementAuto || dataPlacement == PlacementHeader {
+			var headerPayloadChunks []string
+			for i := 0; true; i++ {
+				chunk := request.Header.Get(fmt.Sprintf("%s-%d", uplinkDataKey, i))
+				if chunk == "" {
+					break
+				}
+				headerPayloadChunks = append(headerPayloadChunks, chunk)
+			}
+			headerPayloadEncoded := strings.Join(headerPayloadChunks, "")
+			headerPayload, err = base64.RawURLEncoding.DecodeString(headerPayloadEncoded)
+			if err != nil {
+				newError("Invalid base64 in header's payload").Base(err).AtInfo().WriteToLog()
+				writer.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		var cookiePayload []byte
+		if dataPlacement == PlacementAuto || dataPlacement == PlacementCookie {
+			var cookiePayloadChunks []string
+			for i := 0; true; i++ {
+				cookieName := fmt.Sprintf("%s_%d", uplinkDataKey, i)
+				if c, _ := request.Cookie(cookieName); c != nil {
+					cookiePayloadChunks = append(cookiePayloadChunks, c.Value)
+				} else {
+					break
+				}
+			}
+			cookiePayloadEncoded := strings.Join(cookiePayloadChunks, "")
+			cookiePayload, err = base64.RawURLEncoding.DecodeString(cookiePayloadEncoded)
+			if err != nil {
+				newError("Invalid base64 in cookies' payload").Base(err).AtInfo().WriteToLog()
+				writer.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		var bodyPayload []byte
+		if dataPlacement == PlacementAuto || dataPlacement == PlacementBody {
+			bodyPayload, err = io.ReadAll(request.Body)
+			if err != nil {
+				newError("failed to upload (ReadAll)").Base(err).AtInfo().WriteToLog()
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+
+		payload := slices.Concat(headerPayload, cookiePayload, bodyPayload)
+
+		seq, err := strconv.ParseUint(seqStr, 10, 64)
+		if err != nil {
+			newError("failed to upload (ParseUint)").Base(err).AtInfo().WriteToLog()
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		err = currentSession.uploadQueue.Push(Packet{
+			Payload: payload,
+			Seq:     seq,
+		})
+
+		if err != nil {
+			newError("failed to upload (PushPayload)").Base(err).AtInfo().WriteToLog()
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if len(bodyPayload) == 0 {
+			// Methods without a body are usually cached by default.
+			writer.Header().Set("Cache-Control", "no-store")
+		}
+
+		writer.WriteHeader(http.StatusOK)
+	} else if request.Method == "GET" || sessionId == "" { // stream-down, stream-one
+		if sessionId != "" {
+			// after GET is done, the connection is finished. disable automatic
+			// session reaping, and handle it in defer
+			currentSession.isFullyConnected.Close()
+			defer h.sessions.Delete(sessionId)
+		}
+
+		// magic header instructs nginx + apache to not buffer response body
+		writer.Header().Set("X-Accel-Buffering", "no")
+		// A web-compliant header telling all middleboxes to disable caching.
+		// Should be able to prevent overloading the cache, or stop CDNs from
+		// teeing the response stream into their cache, causing slowdowns.
+		writer.Header().Set("Cache-Control", "no-store")
+		// magic header to make the HTTP middle box consider this as SSE to disable buffer
+		writer.Header().Set("Content-Type", "text/event-stream")
+
+		writer.WriteHeader(http.StatusOK)
+		writer.(http.Flusher).Flush()
+
+		httpSC := &httpServerConn{
+			Instance:       done.New(),
+			Reader:         request.Body,
+			ResponseWriter: writer,
+		}
+		conn := &splitConn{
+			writer:     httpSC,
+			reader:     httpSC,
+			remoteAddr: remoteAddr,
+			localAddr:  h.localAddr,
+		}
+		if sessionId != "" { // if not stream-one
+			conn.reader = currentSession.uploadQueue
+		}
+
+		h.ln.addConn(conn)
+
+		// "A ResponseWriter may not be used after [Handler.ServeHTTP] has returned."
+		select {
+		case <-request.Context().Done():
+		case <-httpSC.Wait():
+		}
+
+		conn.Close()
+	} else {
+		newError("unsupported method: ", request.Method).AtInfo().WriteToLog()
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+type httpServerConn struct {
+	sync.Mutex
+	*done.Instance
+	io.Reader // no need to Close request.Body
+	http.ResponseWriter
+}
+
+func (c *httpServerConn) Write(b []byte) (int, error) {
+	c.Lock()
+	defer c.Unlock()
+	if c.Done() {
+		return 0, io.ErrClosedPipe
+	}
+	n, err := c.ResponseWriter.Write(b)
+	if err == nil {
+		c.ResponseWriter.(http.Flusher).Flush()
+	}
+	return n, err
+}
+
+func (c *httpServerConn) Close() error {
+	c.Lock()
+	defer c.Unlock()
+	return c.Instance.Close()
+}
+
+type Listener struct {
+	sync.Mutex
+	server     http.Server
+	h3server   *http3.Server
+	listener   net.Listener
+	h3listener *quic.EarlyListener
+	config     *Config
+	addConn    internet.ConnHandler
+}
+
+func ListenSH(ctx context.Context, address net.Address, port net.Port, streamSettings *internet.MemoryStreamConfig, addConn internet.ConnHandler) (internet.Listener, error) {
+	l := &Listener{
+		addConn: addConn,
+	}
+	l.config = streamSettings.ProtocolSettings.(*Config)
+	var err error
+	handler := &requestHandler{
+		config:    l.config,
+		host:      l.config.Host,
+		path:      l.config.GetNormalizedPath(),
+		ln:        l,
+		sessionMu: &sync.Mutex{},
+		sessions:  sync.Map{},
+	}
+
+	tlsConfig := tls.ConfigFromStreamSettings(streamSettings)
+	realityConfig := reality.ConfigFromStreamSettings(streamSettings)
+
+	if decideHTTPVersion(tlsConfig, realityConfig) == "3" { // quic
+		conn, err := internet.ListenSystemPacket(context.Background(), &net.UDPAddr{
+			IP:   address.IP(),
+			Port: int(port),
+		}, streamSettings.SocketSettings)
+		if err != nil {
+			return nil, newError("failed to listen UDP for XHTTP/3 on ", address).Base(err)
+		}
+		l.h3listener, err = quic.ListenEarly(conn, tlsConfig.GetTLSConfig(), nil)
+		if err != nil {
+			return nil, newError("failed to listen QUIC for XHTTP/3 on ", address, ":", port).Base(err)
+		}
+		handler.localAddr = l.h3listener.Addr()
+		l.h3server = &http3.Server{
+			Handler: handler,
+		}
+		go func() {
+			if err := l.h3server.ServeListener(l.h3listener); err != nil {
+				newError("failed to serve HTTP/3 for XHTTP/3").Base(err).AtWarning().WriteToLog(session.ExportIDToError(ctx))
+			}
+		}()
+		newError("listening QUIC for XHTTP/3 on ", address, ":", port).WriteToLog(session.ExportIDToError(ctx))
+
+		return l, err
+	}
+
+	if port == net.Port(0) { // unix
+		l.listener, err = internet.ListenSystem(ctx, &net.UnixAddr{
+			Name: address.Domain(),
+			Net:  "unix",
+		}, streamSettings.SocketSettings)
+		if err != nil {
+			return nil, newError("failed to listen UNIX domain socket for XHTTP on ", address).Base(err)
+		}
+		newError("listening UNIX domain socket for XHTTP on ", address).WriteToLog(session.ExportIDToError(ctx))
+	} else { // tcp
+		l.listener, err = internet.ListenSystem(ctx, &net.TCPAddr{
+			IP:   address.IP(),
+			Port: int(port),
+		}, streamSettings.SocketSettings)
+		if err != nil {
+			return nil, newError("failed to listen TCP for XHTTP on ", address, ":", port).Base(err)
+		}
+		newError("listening TCP for XHTTP on ", address, ":", port).WriteToLog(session.ExportIDToError(ctx))
+	}
+	if tlsConfig != nil {
+		l.listener = gotls.NewListener(l.listener, tlsConfig.GetTLSConfig())
+	}
+	if realityConfig != nil {
+		l.listener = utls.NewRealityListener(l.listener, realityConfig.GetREALITYConfig())
+	}
+
+	handler.localAddr = l.listener.Addr()
+
+	// server can handle both plaintext HTTP/1.1 and h2c
+	protocols := new(http.Protocols)
+	protocols.SetHTTP1(true)
+	protocols.SetUnencryptedHTTP2(true)
+	l.server = http.Server{
+		Handler:           handler,
+		ReadHeaderTimeout: time.Second * 4,
+		Protocols:         protocols,
+	}
+	go func() {
+		if err := l.server.Serve(l.listener); err != nil {
+			newError("failed to serve HTTP for XHTTP").Base(err).AtWarning().WriteToLog(session.ExportIDToError(ctx))
+		}
+	}()
+	return l, err
+}
+
+// Addr implements net.Listener.Addr().
+func (ln *Listener) Addr() net.Addr {
+	if ln.h3listener != nil {
+		return ln.h3listener.Addr()
+	}
+	return ln.listener.Addr()
+}
+
+// Close implements net.Listener.Close().
+func (ln *Listener) Close() error {
+	if ln.h3server != nil {
+		return ln.h3server.Close()
+	}
+	return ln.listener.Close()
+}
+
+func init() {
+	common.Must(internet.RegisterTransportListener(protocolName, ListenSH))
+}
