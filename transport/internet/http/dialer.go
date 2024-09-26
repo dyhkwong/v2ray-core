c4b3dca050bdebbbb8f23e47b1f14ae62eaab6f1
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2024-09-26 15:04:59 +0800

    import http3 transport

--- transport/internet/http/dialer.go
+++ transport/internet/http/dialer.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"net/url"
 	"sync"
+	"time"
 
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
 	"golang.org/x/net/http2"
 
 	core "github.com/v2fly/v2ray-core/v5"
@@ -15,9 +18,12 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/net/cnc"
+	"github.com/v2fly/v2ray-core/v5/common/uuid"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/reality"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/security"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tls"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tls/utls"
 	"github.com/v2fly/v2ray-core/v5/transport/pipe"
 )
 
@@ -32,6 +38,33 @@ var (
 )
 
 type dialerCanceller func()
+
+type connWrapper struct {
+	net.Conn
+	localAddr net.Addr
+}
+
+func (c *connWrapper) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	n, err = c.Read(p)
+	return n, c.RemoteAddr(), err
+}
+
+func (c *connWrapper) WriteTo(p []byte, _ net.Addr) (n int, err error) {
+	return c.Write(p)
+}
+
+func (c *connWrapper) LocalAddr() net.Addr {
+	return c.localAddr
+}
+
+func NewConnWrapper(conn net.Conn) net.PacketConn {
+	// https://github.com/quic-go/quic-go/commit/8189e75be6121fdc31dc1d6085f17015e9154667#diff-4c6aaadced390f3ce9bec0a9c9bb5203d5fa85df79023e3e0eec423dc9baa946R48-R62
+	uuid := uuid.New()
+	return &connWrapper{
+		Conn:      conn,
+		localAddr: &net.UnixAddr{Name: uuid.String()},
+	}
+}
 
 func getHTTPClient(ctx context.Context, dest net.Destination, securityEngine *security.Engine, streamSettings *internet.MemoryStreamConfig) (*http.Client, dialerCanceller) {
 	globalDialerAccess.Lock()
@@ -51,7 +84,17 @@ func getHTTPClient(ctx context.Context, dest net.Destination, securityEngine *se
 		return client, canceller
 	}
 
-	transport := &http2.Transport{
+	var tlsConfig *tls.Config
+	switch cfg := streamSettings.SecuritySettings.(type) {
+	case *tls.Config:
+		tlsConfig = cfg
+	case *utls.Config:
+		tlsConfig = cfg.GetTlsConfig()
+	}
+	isH3 := tlsConfig != nil && len(tlsConfig.NextProtocol) == 1 && tlsConfig.NextProtocol[0] == "h3"
+
+	var transport http.RoundTripper
+	transport = &http2.Transport{
 		DialTLSContext: func(_ context.Context, network, addr string, tlsConfig *gotls.Config) (gonet.Conn, error) {
 			rawHost, rawPort, err := net.SplitHostPort(addr)
 			if err != nil {
@@ -98,6 +141,37 @@ func getHTTPClient(ctx context.Context, dest net.Destination, securityEngine *se
 		},
 	}
 
+	if isH3 {
+		transport = &http3.RoundTripper{
+			QUICConfig: &quic.Config{
+				MaxIdleTimeout:     300 * time.Second,
+				MaxIncomingStreams: -1,
+				KeepAlivePeriod:    10 * time.Second,
+			},
+			TLSClientConfig: tlsConfig.GetTLSConfig(tls.WithDestination(dest)),
+			Dial: func(_ context.Context, addr string, tlsCfg *gotls.Config, cfg *quic.Config) (quic.EarlyConnection, error) {
+				detachedContext := core.ToBackgroundDetachedContext(ctx)
+				conn, err := internet.DialSystem(detachedContext, dest, streamSettings.SocketSettings)
+				if err != nil {
+					return nil, err
+				}
+				var packetConn net.PacketConn
+				switch c := conn.(type) {
+				case *internet.PacketConnWrapper:
+					packetConn = c.Conn
+				case net.PacketConn:
+					packetConn = c
+				default:
+					packetConn = NewConnWrapper(conn)
+				}
+				tr := quic.Transport{
+					Conn: packetConn,
+				}
+				return tr.DialEarly(detachedContext, conn.RemoteAddr(), tlsCfg, cfg)
+			},
+		}
+	}
+
 	client := &http.Client{
 		Transport: transport,
 	}
@@ -109,6 +183,19 @@ func getHTTPClient(ctx context.Context, dest net.Destination, securityEngine *se
 // Dial dials a new TCP connection to the given destination.
 func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	httpSettings := streamSettings.ProtocolSettings.(*Config)
+
+	var tlsConfig *tls.Config
+	switch cfg := streamSettings.SecuritySettings.(type) {
+	case *tls.Config:
+		tlsConfig = cfg
+	case *utls.Config:
+		tlsConfig = cfg.GetTlsConfig()
+	}
+	isH3 := tlsConfig != nil && len(tlsConfig.NextProtocol) == 1 && tlsConfig.NextProtocol[0] == "h3"
+	if isH3 {
+		dest.Network = net.Network_UDP
+	}
+
 	securityEngine, _ := security.CreateSecurityEngineFromSettings(ctx, streamSettings)
 	realityConfig := reality.ConfigFromStreamSettings(streamSettings)
 	if securityEngine == nil && realityConfig == nil {
@@ -142,11 +229,15 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 			Host:   dest.NetAddr(),
 			Path:   httpSettings.getNormalizedPath(),
 		},
-		Proto:      "HTTP/2",
-		ProtoMajor: 2,
-		ProtoMinor: 0,
-		Header:     httpHeaders,
+		Header: httpHeaders,
 	}
+
+	if !isH3 {
+		request.Proto = "HTTP/2"
+		request.ProtoMajor = 2
+		request.ProtoMinor = 0
+	}
+
 	// Disable any compression method from server.
 	request.Header.Set("Accept-Encoding", "identity")
 
