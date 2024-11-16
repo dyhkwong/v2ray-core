f1d427363d65a2e68735e53c250aa72184e759aa
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-11-16 14:20:30 +0800

    Hysteria 2 port hopping

--- transport/internet/hysteria2/dialer.go
+++ transport/internet/hysteria2/dialer.go
@@ -3,10 +3,12 @@ package hysteria2
 import (
 	"context"
 	"sync"
+	"time"
 
 	hyClient "github.com/apernet/hysteria/core/v2/client"
 	hyProtocol "github.com/apernet/hysteria/core/v2/international/protocol"
 	"github.com/apernet/hysteria/extras/v2/obfs"
+	"github.com/apernet/hysteria/extras/v2/transport/udphop"
 	"github.com/apernet/quic-go/quicvarint"
 
 	"github.com/v2fly/v2ray-core/v5/common"
@@ -94,8 +96,34 @@ func NewHyClient(ctx context.Context, dest net.Destination, streamSettings *inte
 		BandwidthConfig: hyClient.BandwidthConfig{MaxTx: config.Congestion.GetUpMbps() * MBps, MaxRx: config.GetCongestion().GetDownMbps() * MBps},
 	}
 
+	if len(config.HopPorts) > 0 {
+		host, _, err := net.SplitHostPort(serverAddr.String())
+		if err != nil {
+			return nil, err
+		}
+		udpHopAddr, err := udphop.ResolveUDPHopAddr(net.JoinHostPort(host, config.HopPorts))
+		if err != nil {
+			return nil, err
+		}
+		hyConfig.ServerAddr = udpHopAddr
+	}
+
 	connFactory := &connFactory{
 		NewFunc: func(addr net.Addr) (net.PacketConn, error) {
+			if len(config.HopPorts) > 0 {
+				return udphop.NewUDPHopPacketConn(addr.(*udphop.UDPHopAddr), time.Duration(config.HopInterval)*time.Second,
+					func() (net.PacketConn, error) {
+						rawConn, err := internet.DialSystem(ctx, net.DestinationFromAddr(serverAddr), streamSettings.SocketSettings)
+						if err != nil {
+							return nil, newError("failed to dial to dest: ", err).AtWarning().Base(err)
+						}
+						if _, ok := rawConn.(net.PacketConn); !ok {
+							return nil, newError("port hopping does not work with chain proxy")
+						}
+						return internet.WrapPacketConn(rawConn), nil
+					},
+				)
+			}
 			rawConn, err := internet.DialSystem(ctx, net.DestinationFromAddr(addr), streamSettings.SocketSettings)
 			if err != nil {
 				return nil, newError("failed to dial to dest: ", err).AtWarning().Base(err)
