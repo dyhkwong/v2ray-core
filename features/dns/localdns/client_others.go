2f3b5f9df7424f3113491d04f7336558302efe56
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-12-15 13:34:35 +0800

    Support non-A/AAAA DNS queries

---
+++ features/dns/localdns/client_others.go
@@ -0,0 +1,105 @@
+//go:build !(android && cgo)
+
+package localdns
+
+import (
+	"context"
+	"encoding/binary"
+	"time"
+
+	"golang.org/x/net/dns/dnsmessage"
+
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/bytespool"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+)
+
+var rawQueryFunc = func(request []byte) ([]byte, error) {
+	requestMsg := new(dnsmessage.Message)
+	err := requestMsg.Unpack(request)
+	if err != nil {
+		return nil, newError("failed to parse dns request").Base(err)
+	}
+
+	dns := dnsReadConfig()
+	done := make(chan any)
+	var udpConn *net.UDPConn
+	go func() {
+		udpConn, err = net.DialUDP("udp", nil, &net.UDPAddr{
+			IP:   dns[0].AsSlice(),
+			Port: 53,
+			Zone: dns[0].Zone(),
+		})
+		close(done)
+	}()
+	select {
+	case <-time.After(time.Second * 5):
+		return nil, context.DeadlineExceeded
+	case <-done:
+		if err != nil {
+			return nil, err
+		}
+		defer udpConn.Close()
+	}
+
+	if _, err := udpConn.Write(request); err != nil {
+		return nil, err
+	}
+
+	response := bytespool.Alloc(buf.Size)
+	defer bytespool.Free(response)
+	n, err := udpConn.Read(response)
+	if err != nil {
+		return nil, err
+	}
+	if n < 12 {
+		return nil, newError("response too short")
+	}
+	if binary.BigEndian.Uint16(response[:2]) != requestMsg.ID {
+		return nil, newError("DNS message ID mismatch")
+	}
+	if response[3]&0x02 < 0x02 {
+		return response[:n], nil
+	}
+
+	newError("truncated, retry over TCP").AtError().WriteToLog()
+	done = make(chan any)
+	var tcpConn *net.TCPConn
+	go func() {
+		tcpConn, err = net.DialTCP("tcp", nil, &net.TCPAddr{
+			IP:   dns[0].AsSlice(),
+			Port: 53,
+			Zone: dns[0].Zone(),
+		})
+		close(done)
+	}()
+	select {
+	case <-time.After(time.Second * 5):
+		return nil, context.DeadlineExceeded
+	case <-done:
+		if err != nil {
+			return nil, err
+		}
+		defer tcpConn.Close()
+	}
+
+	reqBuf := buf.NewWithSize(2 + int32(len(request)))
+	defer reqBuf.Release()
+	binary.Write(reqBuf, binary.BigEndian, uint16(len(request)))
+	reqBuf.Write(request)
+	if _, err := tcpConn.Write(reqBuf.Bytes()); err != nil {
+		return nil, err
+	}
+
+	var length uint16
+	if err := binary.Read(tcpConn, binary.BigEndian, &length); err != nil {
+		return nil, err
+	}
+	respBuf := buf.NewWithSize(int32(length))
+	defer respBuf.Release()
+
+	if _, err := respBuf.ReadFullFrom(tcpConn, int32(length)); err != nil {
+		return nil, err
+	}
+	return respBuf.Bytes(), nil
+}
