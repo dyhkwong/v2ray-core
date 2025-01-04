39964a37735996a1c6c42a1e6094b84bbd9e87a3
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-01-04 15:26:55 +0800

    Add DNS sniffer

--- app/dns/nameserver_udp.go
+++ app/dns/nameserver_udp.go
@@ -289,7 +289,7 @@ func (s *ClassicNameServer) sendQuery(ctx context.Context, domain string, client
 			udpCtx = session.ContextWithInbound(udpCtx, inbound)
 		}
 		udpCtx = session.ContextWithContent(udpCtx, &session.Content{
-			Protocol:       "dns",
+			Protocol:       "v2ray.dns",
 			SkipDNSResolve: true,
 		})
 		s.udpServer.Dispatch(udpCtx, s.address, b)
@@ -317,7 +317,7 @@ func (s *ClassicNameServer) QueryRaw(originCtx context.Context, request []byte) 
 		dnsCtx = session.ContextWithInbound(dnsCtx, inbound)
 	}
 	dnsCtx = session.ContextWithContent(dnsCtx, &session.Content{
-		Protocol:       "dns",
+		Protocol:       "v2ray.dns",
 		SkipDNSResolve: true,
 	})
 	var cancel context.CancelFunc
