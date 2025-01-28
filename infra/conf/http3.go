e861acf1f728b376def0334b565d05c2fa668cd6
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-01-28 22:31:33 +0800

    Add HTTP3 proxy client

---
+++ infra/conf/http3.go
@@ -0,0 +1,31 @@
+package conf
+
+import (
+	"github.com/golang/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v4/infra/conf/cfgcommon"
+	"github.com/v2fly/v2ray-core/v4/proxy/http3"
+)
+
+type HTTP3ClientConfig struct {
+	Address  *cfgcommon.Address `json:"address"`
+	Port     uint16             `json:"port"`
+	Level    byte               `json:"level"`
+	Username string             `json:"username"`
+	Password string             `json:"password"`
+	Headers  map[string]string  `json:"headers"`
+}
+
+func (c *HTTP3ClientConfig) Build() (proto.Message, error) {
+	if c.Address == nil {
+		return nil, newError("missing server address")
+	}
+	return &http3.ClientConfig{
+		Address:  c.Address.Build(),
+		Port:     uint32(c.Port),
+		Level:    uint32(c.Level),
+		Username: c.Username,
+		Password: c.Password,
+		Headers:  c.Headers,
+	}, nil
+}
