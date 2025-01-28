cc091c3f11164d3f0443a1942724d5121451b325
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-01-28 22:31:33 +0800

    Add HTTP3 proxy client

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -43,6 +43,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/proxy/dokodemo"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/freedom"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/http"
+	_ "github.com/v2fly/v2ray-core/v4/proxy/http3"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/loopback"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/mixed"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/mtproto"
