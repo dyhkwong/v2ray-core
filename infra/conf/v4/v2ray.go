d419882ad8385060c55dc563efdbd1ff93ca2bd5
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-02-19 20:28:43 +0800

    Import AnyTLS

--- infra/conf/v4/v2ray.go
+++ infra/conf/v4/v2ray.go
@@ -39,6 +39,7 @@ var (
 		"shadowsocks-2022-relay": func() interface{} { return new(Shadowsocks2022RelayServerConfig) },
 		"mixed":                  func() interface{} { return new(MixedServerConfig) },
 		"wireguard":              func() interface{} { return new(WireGuardServerConfig) },
+		"anytls":                 func() interface{} { return new(AnyTLSServerConfig) },
 	}, "protocol", "settings")
 
 	outboundConfigLoader = loader.NewJSONConfigLoader(loader.ConfigCreatorCache{
@@ -60,6 +61,7 @@ var (
 		"shadowsocks-2022": func() interface{} { return new(Shadowsocks2022ClientConfig) },
 		"wireguard":        func() interface{} { return new(WireGuardClientConfig) },
 		"shadowtls":        func() interface{} { return new(ShadowTLSClientConfig) },
+		"anytls":           func() interface{} { return new(AnyTLSClientConfig) },
 	}, "protocol", "settings")
 )
 
