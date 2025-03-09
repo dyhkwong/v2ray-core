84f78e234656255982a61f93045f65ff424af8ed
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-03-10 02:35:32 +0800

    Fix buffer release

--- proxy/http/client.go
+++ proxy/http/client.go
@@ -222,6 +222,7 @@ func setUpHTTPTunnel(ctx context.Context, dest net.Destination, target string, u
 		if bufferedReader.Buffered() > 0 {
 			payload, err := buf.ReadFrom(io.LimitReader(bufferedReader, int64(bufferedReader.Buffered())))
 			if err != nil {
+				rawConn.Close()
 				return nil, nil, newError("unable to drain buffer: ").Base(err)
 			}
 			return rawConn, payload, nil
@@ -309,7 +310,6 @@ func setUpHTTPTunnel(ctx context.Context, dest net.Destination, target string, u
 
 		proxyConn, err := connectHTTP2(rawConn, h2clientConn)
 		if err != nil {
-			rawConn.Close()
 			return nil, nil, err
 		}
 
@@ -326,6 +326,7 @@ func setUpHTTPTunnel(ctx context.Context, dest net.Destination, target string, u
 
 		return proxyConn, nil, err
 	default:
+		rawConn.Close()
 		return nil, nil, newError("negotiated unsupported application layer protocol: " + nextProto)
 	}
 }
