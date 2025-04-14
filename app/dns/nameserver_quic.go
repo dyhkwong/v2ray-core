17427cdc05695455bd5ff77186d73ea4b2c146bd
patterniha <71074308+patterniha@users.noreply.github.com>
2025-04-14 22:33:10 +0800

    Fix DNS with cache disabled

--- app/dns/nameserver_quic.go
+++ app/dns/nameserver_quic.go
@@ -319,15 +319,15 @@ func (s *QUICNameServer) QueryIP(ctx context.Context, domain string, clientIP ne
 	s.sendQuery(ctx, fqdn, clientIP, option)
 
 	for {
-		ips, err := s.findIPsForDomain(fqdn, option)
-		if err != errRecordNotFound {
-			return ips, err
-		}
-
 		select {
 		case <-ctx.Done():
 			return nil, ctx.Err()
 		case <-done:
+		}
+
+		ips, err := s.findIPsForDomain(fqdn, option)
+		if err != errRecordNotFound {
+			return ips, err
 		}
 	}
 }
