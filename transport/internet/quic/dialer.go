9a55fbab6272b37a928a09702729120d0961edb7
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-05-23 17:42:25 +0800

    Improve localdns client

--- transport/internet/quic/dialer.go
+++ transport/internet/quic/dialer.go
@@ -12,6 +12,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/task"
+	"github.com/v2fly/v2ray-core/v4/features/dns/localdns"
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/tls"
 )
@@ -215,11 +216,14 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 			Port: int(dest.Port),
 		}
 	} else {
-		addr, err := net.ResolveUDPAddr("udp", dest.NetAddr())
+		addr, err := localdns.New().LookupIP(dest.Address.Domain())
 		if err != nil {
 			return nil, err
 		}
-		destAddr = addr
+		destAddr = &net.UDPAddr{
+			IP:   addr[0],
+			Port: int(dest.Port),
+		}
 	}
 
 	return client.openConnection(destAddr, streamSettings)
