732f4cef542ae492cf493558f68ffe236fb5e2b2
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-05-27 04:33:52 +0800

    Add hook for Android local DNS

--- features/dns/localdns/client_android.go
+++ features/dns/localdns/client_android.go
@@ -1,44 +1,24 @@
-//go:build android && cgo
-
 package localdns
 
-// #include <stdlib.h>
-// #include <android/multinetwork.h>
-import "C"
-
 import (
-	"context"
-
 	"golang.org/x/net/dns/dnsmessage"
-	"golang.org/x/sys/unix"
-
-	"github.com/v2fly/v2ray-core/v5/common/buf"
-	"github.com/v2fly/v2ray-core/v5/common/bytespool"
 )
 
-var rawQueryFunc = func(request []byte) ([]byte, error) {
+var defaultRawQueryFunc = func(request []byte) ([]byte, error) {
 	requestMsg := new(dnsmessage.Message)
 	err := requestMsg.Unpack(request)
 	if err != nil {
 		return nil, newError("failed to parse dns request").Base(err)
 	}
-
-	fd := C.android_res_nsend(0, (*C.uint8_t)(&request[0]), C.size_t(len(request)), 0)
-	if fd < 0 {
-		return nil, unix.Errno(-fd)
-	}
-	nReady, err := unix.Poll([]unix.PollFd{{Fd: int32(fd), Events: unix.EPOLLIN | unix.EPOLLERR}}, 5000)
-	if err != nil {
-		return nil, err
-	}
-	if nReady == 0 {
-		return nil, context.DeadlineExceeded
-	}
-	response := make([]byte, buf.Size)
-	var rcode C.int
-
-	if n := C.android_res_nresult(fd, &rcode, (*C.uint8_t)(&response[0]), C.size_t(len(response))); n < 0 {
-		return nil, unix.Errno(-n)
-	}
-	return response[:n], nil
+	newError("unsupported: need Android 10 or higher").AtError().WriteToLog()
+	/*
+		responseMsg := new(dnsmessage.Message)
+		responseMsg.ID = requestMsg.ID
+		responseMsg.RCode = dnsmessage.RCodeNotImplemented
+		responseMsg.RecursionAvailable = true
+		responseMsg.RecursionDesired = true
+		responseMsg.Response = true
+		return responseMsg.Pack()
+	*/
+	return []byte{request[0], request[1], 0x81, 0x84, 0, 0, 0, 0, 0, 0, 0, 0}, nil
 }
