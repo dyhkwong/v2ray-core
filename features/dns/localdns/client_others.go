e657402433372314679605e933853ef00166c5a8
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-05-27 04:33:52 +0800

    Add hook for Android local DNS

--- features/dns/localdns/client_others.go
+++ features/dns/localdns/client_others.go
@@ -1,4 +1,4 @@
-//go:build !(android && cgo)
+//go:build !android
 
 package localdns
 
@@ -14,7 +14,7 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common/net"
 )
 
-var rawQueryFunc = func(request []byte) ([]byte, error) {
+var defaultRawQueryFunc = func(request []byte) ([]byte, error) {
 	requestMsg := new(dnsmessage.Message)
 	err := requestMsg.Unpack(request)
 	if err != nil {
