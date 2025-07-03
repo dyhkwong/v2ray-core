9f7bb2d3a6be7e513de098f9996bcae016a506e3
Shelikhoo <xiaokangwang@outlook.com>
2025-07-03 23:30:11 +0100

    fix corrupted tls12 aead stream in tls mirror

--- transport/internet/tlsmirror/mirrorbase/conn.go
+++ transport/internet/tlsmirror/mirrorbase/conn.go
@@ -173,7 +173,7 @@ func (c *conn) c2sWorker() {
 			// memory consistency synchronization for value c.tls12ExplicitNonce is required!!!
 			if *c.tls12ExplicitNonce {
 				if record.RecordType == mirrorcommon.TLSRecord_RecordType_application_data {
-					nonce := c.s2cExplicitNonceCounterGenerator()
+					nonce := c.c2sExplicitNonceCounterGenerator()
 					copy(record.Fragment, nonce)
 				}
 			}
@@ -220,7 +220,7 @@ func (c *conn) c2sWorker() {
 				return
 			}
 
-			c.c2sExplicitNonceCounterGenerator = crypto.GenerateIncreasingNonce([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+			c.c2sExplicitNonceCounterGenerator = reverseBytesGeneratorByteOrder(crypto.GenerateIncreasingNonce([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}))
 		}
 
 		if c.OnC2SMessage != nil {
@@ -294,7 +294,7 @@ func (c *conn) s2cWorker() {
 			// memory consistency synchronization for value c.tls12ExplicitNonce is required!!!
 			if *c.tls12ExplicitNonce {
 				if record.RecordType == mirrorcommon.TLSRecord_RecordType_application_data {
-					nonce := c.c2sExplicitNonceCounterGenerator()
+					nonce := c.s2cExplicitNonceCounterGenerator()
 					copy(record.Fragment, nonce)
 				}
 			}
@@ -332,7 +332,7 @@ func (c *conn) s2cWorker() {
 				c.done()
 				return
 			}
-			c.c2sExplicitNonceCounterGenerator = crypto.GenerateIncreasingNonce([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+			c.s2cExplicitNonceCounterGenerator = reverseBytesGeneratorByteOrder(crypto.GenerateIncreasingNonce([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}))
 		}
 
 		if c.OnS2CMessage != nil {
---
+++ transport/internet/tlsmirror/mirrorbase/crypto.go
@@ -0,0 +1,22 @@
+//go:build !confonly
+// +build !confonly
+
+package mirrorbase
+
+import (
+	"github.com/v2fly/v2ray-core/v4/common/crypto"
+)
+
+func reverseBytesGeneratorByteOrder(generator crypto.BytesGenerator) crypto.BytesGenerator {
+	var reverseResult [8]byte
+	return func() []byte {
+		result := generator()
+		if len(result) != 8 {
+			panic("reverseBytesGeneratorByteOrder requires a generator that returns exactly 8 bytes")
+		}
+		for i := 0; i < 8; i++ {
+			reverseResult[i] = result[7-i]
+		}
+		return reverseResult[:]
+	}
+}
