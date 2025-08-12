4d51e1c404379077212bc0a55b90fd1f06e88df1
RPRX <63339210+rprx@users.noreply.github.com>
2025-08-12 16:08:56 +0800

    Import VLESS encryption

--- proxy/vless/outbound/config.pb.go
+++ proxy/vless/outbound/config.pb.go
@@ -77,6 +77,7 @@ type SimplifiedConfig struct {
 	Port           uint32                    `protobuf:"varint,2,opt,name=port,proto3" json:"port,omitempty"`
 	Uuid           string                    `protobuf:"bytes,3,opt,name=uuid,proto3" json:"uuid,omitempty"`
 	PacketEncoding packetaddr.PacketAddrType `protobuf:"varint,4,opt,name=packet_encoding,json=packetEncoding,proto3,enum=v2ray.core.net.packetaddr.PacketAddrType" json:"packet_encoding,omitempty"`
+	Encryption     string                    `protobuf:"bytes,5,opt,name=encryption,proto3" json:"encryption,omitempty"`
 	unknownFields  protoimpl.UnknownFields
 	sizeCache      protoimpl.SizeCache
 }
@@ -139,6 +140,13 @@ func (x *SimplifiedConfig) GetPacketEncoding() packetaddr.PacketAddrType {
 	return packetaddr.PacketAddrType(0)
 }
 
+func (x *SimplifiedConfig) GetEncryption() string {
+	if x != nil {
+		return x.Encryption
+	}
+	return ""
+}
+
 var File_proxy_vless_outbound_config_proto protoreflect.FileDescriptor
 
 const file_proxy_vless_outbound_config_proto_rawDesc = "" +
@@ -146,12 +154,15 @@ const file_proxy_vless_outbound_config_proto_rawDesc = "" +
 	"!proxy/vless/outbound/config.proto\x12\x1fv2ray.core.proxy.vless.outbound\x1a!common/protocol/server_spec.proto\x1a\x18common/net/address.proto\x1a common/protoext/extensions.proto\x1a\"common/net/packetaddr/config.proto\"\x9e\x01\n" +
 	"\x06Config\x12@\n" +
 	"\x05vnext\x18\x01 \x03(\v2*.v2ray.core.common.protocol.ServerEndpointR\x05vnext\x12R\n" +
-	"\x0fpacket_encoding\x18\x02 \x01(\x0e2).v2ray.core.net.packetaddr.PacketAddrTypeR\x0epacketEncoding\"\xe2\x01\n" +
+	"\x0fpacket_encoding\x18\x02 \x01(\x0e2).v2ray.core.net.packetaddr.PacketAddrTypeR\x0epacketEncoding\"\x82\x02\n" +
 	"\x10SimplifiedConfig\x12;\n" +
 	"\aaddress\x18\x01 \x01(\v2!.v2ray.core.common.net.IPOrDomainR\aaddress\x12\x12\n" +
 	"\x04port\x18\x02 \x01(\rR\x04port\x12\x12\n" +
 	"\x04uuid\x18\x03 \x01(\tR\x04uuid\x12R\n" +
-	"\x0fpacket_encoding\x18\x04 \x01(\x0e2).v2ray.core.net.packetaddr.PacketAddrTypeR\x0epacketEncoding:\x15\x82\xb5\x18\x11\n" +
+	"\x0fpacket_encoding\x18\x04 \x01(\x0e2).v2ray.core.net.packetaddr.PacketAddrTypeR\x0epacketEncoding\x12\x1e\n" +
+	"\n" +
+	"encryption\x18\x05 \x01(\tR\n" +
+	"encryption:\x15\x82\xb5\x18\x11\n" +
 	"\boutbound\x12\x05vlessB~\n" +
 	"#com.v2ray.core.proxy.vless.outboundP\x01Z3github.com/v2fly/v2ray-core/v5/proxy/vless/outbound\xaa\x02\x1fV2Ray.Core.Proxy.Vless.Outboundb\x06proto3"
 
