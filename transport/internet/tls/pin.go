7ee865e28007c289ddb49993b14a801b74bc54e0
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-09-27 03:11:45 +0800

    Add PinnedPeerCertificatePublicKeySha256 and PinnedPeerCertificateSha256

--- transport/internet/tls/pin.go
+++ transport/internet/tls/pin.go
@@ -2,7 +2,9 @@ package tls
 
 import (
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/pem"
 )
 
@@ -21,6 +23,26 @@ func CalculatePEMCertChainSHA256Hash(certContent []byte) string {
 	return certChainHashB64
 }
 
+func CalculatePEMCertPublicKeySHA256Hash(certContent []byte) (string, error) {
+	block, _ := pem.Decode(certContent)
+	if block == nil {
+		return "", newError("invalid certificate")
+	}
+	certPublicKeyHash, err := GenerateCertPublicKeyHash(block.Bytes)
+	if err != nil {
+		return "", newError("invalid certificate")
+	}
+	return base64.StdEncoding.EncodeToString(certPublicKeyHash), nil
+}
+
+func CalculatePEMCertSHA256Hash(certContent []byte) (string, error) {
+	block, _ := pem.Decode(certContent)
+	if block == nil {
+		return "", newError("invalid certificate")
+	}
+	return hex.EncodeToString(GenerateCertHash(block.Bytes)), nil
+}
+
 func GenerateCertChainHash(rawCerts [][]byte) []byte {
 	var hashValue []byte
 	for _, certValue := range rawCerts {
@@ -33,4 +55,22 @@ func GenerateCertChainHash(rawCerts [][]byte) []byte {
 		}
 	}
 	return hashValue
+}
+
+func GenerateCertPublicKeyHash(rawCert []byte) ([]byte, error) {
+	cert, err := x509.ParseCertificate(rawCert)
+	if err != nil {
+		return nil, newError("invalid certificate")
+	}
+	publicKey, err := x509.MarshalPKIXPublicKey(cert.PublicKey)
+	if err != nil {
+		return nil, newError("invalid public key")
+	}
+	hashValue := sha256.Sum256(publicKey)
+	return hashValue[:], nil
+}
+
+func GenerateCertHash(rawCert []byte) []byte {
+	hashValue := sha256.Sum256(rawCert)
+	return hashValue[:]
 }
