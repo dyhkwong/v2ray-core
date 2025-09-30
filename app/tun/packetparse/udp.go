52046863eb440e58d5d0944328d53ca42973c617
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-09-30 16:55:38 +0800

    Update dependencies and cleanup code

--- app/tun/packetparse/udp.go
+++ app/tun/packetparse/udp.go
@@ -1,5 +1,4 @@
 //go:build !confonly
-// +build !confonly
 
 package packetparse
 
@@ -62,7 +61,7 @@ func TryParseAsUDPPacket(packet []byte) (src, dst net.Destination, data []byte, 
 	src = net.UDPDestination(srcIP, srcPort)
 	dst = net.UDPDestination(dstIP, dstPort)
 	data = udp.Payload
-	return // nolint: nakedret
+	return src, dst, data, err
 }
 
 func TryConstructUDPPacket(src, dst net.Destination, data []byte) ([]byte, error) {
--- common/buf/readv_unix.go
+++ common/buf/readv_unix.go
@@ -1,5 +1,4 @@
 //go:build illumos
-// +build illumos
 
 package buf
 
