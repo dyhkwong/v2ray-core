fce1657ffad1ef0a169fe0a9784731f008b3b6ca
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-09-30 16:50:02 +0800

    Update dependencies and cleanup code

--- transport/internet/sockopt_other.go
+++ transport/internet/sockopt_other.go
@@ -1,24 +1,23 @@
 //go:build js || dragonfly || netbsd || openbsd || solaris
-// +build js dragonfly netbsd openbsd solaris
 
 package internet
 
-func applyOutboundSocketOptions(network string, address string, fd uintptr, config *SocketConfig) error {
+func applyOutboundSocketOptions(_ string, _ string, _ uintptr, _ *SocketConfig) error {
 	return nil
 }
 
-func applyInboundSocketOptions(network string, fd uintptr, config *SocketConfig) error {
+func applyInboundSocketOptions(_ string, _ uintptr, _ *SocketConfig) error {
 	return nil
 }
 
-func bindAddr(fd uintptr, ip []byte, port uint32) error {
+func bindAddr(_ uintptr, _ []byte, _ uint32) error {
 	return nil
 }
 
-func setReuseAddr(fd uintptr) error {
+func setReuseAddr(_ uintptr) error {
 	return nil
 }
 
-func setReusePort(fd uintptr) error {
+func setReusePort(_ uintptr) error {
 	return nil
 }
