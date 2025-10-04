899ef1aece7fa401fd82eabf4294be9dcceafe59
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-10-04 15:17:33 +0800

    Apply transient storage lifecycle receiver framework to more transports

--- app/proxyman/outbound/handler.go
+++ app/proxyman/outbound/handler.go
@@ -430,7 +430,7 @@ func (h *Handler) Close() error {
 
 	if closableProxy, ok := h.proxy.(common.Closable); ok {
 		if err := closableProxy.Close(); err != nil {
-			return newError("unable to close proxy").Base(err)
+			return newError("unable to close proxy")
 		}
 	}
 	return nil
