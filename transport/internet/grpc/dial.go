4e3e3fd0e72be89e0efd8523b0ae607c9c768732
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-10-04 15:17:33 +0800

    Apply transient storage lifecycle receiver framework to more transports

--- transport/internet/grpc/dial.go
+++ transport/internet/grpc/dial.go
@@ -58,7 +58,7 @@ func (t *transportConnectionState) Close() error {
 	for _, conn := range t.scopedDialerMap {
 		_ = conn.Close()
 	}
-	t.scopedDialerMap = nil
+	clear(t.scopedDialerMap)
 	return nil
 }
 
@@ -94,6 +94,7 @@ func dialgRPC(ctx context.Context, dest net.Destination, streamSettings *interne
 	client := encoding.NewGunServiceClient(conn)
 	gunService, err := client.(encoding.GunServiceClientX).TunCustomName(ctx, grpcSettings.ServiceName)
 	if err != nil {
+		conn.Close()
 		canceller()
 		return nil, newError("Cannot dial grpc").Base(err)
 	}
@@ -160,13 +161,8 @@ func getGrpcClient(ctx context.Context, dest net.Destination, dialOption grpc.Di
 		}),
 		grpc.WithDisableServiceConfig(),
 	)
-	canceller = func() {
-		stateTyped.scopedDialerAccess.Lock()
-		defer stateTyped.scopedDialerAccess.Unlock()
-		delete(stateTyped.scopedDialerMap, dialerConf{dest, streamSettings})
-		if err != nil {
-			conn.Close()
-		}
+	if err != nil {
+		return nil, nil, err
 	}
 	stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}] = conn
 	return conn, canceller, err
