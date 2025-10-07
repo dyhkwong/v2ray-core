3b6e5aced539315a8891165b419b535e92cd1540
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-10-08 03:26:29 +0800

    Import sing-box private sing-mux protocol client

--- common/singbridge/dialer.go
+++ common/singbridge/dialer.go
@@ -6,8 +6,14 @@ import (
 	"github.com/sagernet/sing/common/metadata"
 	"github.com/sagernet/sing/common/network"
 
+	core "github.com/v2fly/v2ray-core/v5"
 	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/net/cnc"
+	"github.com/v2fly/v2ray-core/v5/common/session"
+	"github.com/v2fly/v2ray-core/v5/proxy"
+	"github.com/v2fly/v2ray-core/v5/transport"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/pipe"
 )
 
 var _ network.Dialer = (*dialerWrapper)(nil)
@@ -27,5 +33,30 @@ func (d *dialerWrapper) DialContext(ctx context.Context, network string, destina
 }
 
 func (d *dialerWrapper) ListenPacket(ctx context.Context, destination metadata.Socksaddr) (net.PacketConn, error) {
+	panic("invalid")
+}
+
+func NewOutboundDialerWrapper(outbound proxy.Outbound, dialer internet.Dialer) *outboundDialerWrapper {
+	return &outboundDialerWrapper{outbound, dialer}
+}
+
+type outboundDialerWrapper struct {
+	outbound proxy.Outbound
+	dialer   internet.Dialer
+}
+
+func (d *outboundDialerWrapper) DialContext(ctx context.Context, network string, destination metadata.Socksaddr) (net.Conn, error) {
+	ctx = session.ContextWithOutbound(ctx, &session.Outbound{
+		Target: ToDestination(destination, ToNetwork(network)),
+	})
+	opts := []pipe.Option{pipe.WithSizeLimit(64 * 1024)}
+	uplinkReader, uplinkWriter := pipe.New(opts...)
+	downlinkReader, downlinkWriter := pipe.New(opts...)
+	conn := cnc.NewConnection(cnc.ConnectionInputMulti(downlinkWriter), cnc.ConnectionOutputMulti(uplinkReader))
+	go d.outbound.Process(core.ToBackgroundDetachedContext(ctx), &transport.Link{Reader: downlinkReader, Writer: uplinkWriter}, d.dialer)
+	return conn, nil
+}
+
+func (d *outboundDialerWrapper) ListenPacket(ctx context.Context, destination metadata.Socksaddr) (net.PacketConn, error) {
 	panic("invalid")
 }
--- infra/conf/v5cfg/skeleton.go
+++ infra/conf/v5cfg/skeleton.go
@@ -38,6 +38,7 @@ type OutboundConfig struct {
 	StreamSetting      *StreamConfig         `json:"streamSettings"`
 	ProxySettings      *proxycfg.ProxyConfig `json:"proxySettings"`
 	MuxSettings        *muxcfg.MuxConfig     `json:"mux"`
+	SingMuxSettings    *muxcfg.SingMuxConfig `json:"smux"`
 	DomainStrategy     string                `json:"domainStrategy"`
 	DialDomainStrategy string                `json:"dialDomainStrategy"`
 }
