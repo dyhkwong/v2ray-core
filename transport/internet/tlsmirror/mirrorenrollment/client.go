xiaokangwang/v2ray-core-1

finish rtt enrollment

transport/internet/tlsmirror/mirrorenrollment/client.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"net"
 
+	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/environment"
 	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
 	v2net "github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/serial"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorcommon"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorenrollment/httpenrollmentconfirmation"
 )
 
@@ -78,7 +80,9 @@ func (c *EnrollmentConfirmationClient) init() error {
 				return nil, newError("failed to parse destination address").Base(err).AtError()
 			}
 			dest.Network = v2net.Network_TCP
-			return dialer(c.ctx, dest, c.config.PrimaryEgressOutbound)
+
+			loopbackProtectedCtx := mirrorcommon.SetLoopbackProtectionFlagForContext(c.ctx, c.serverIdentity)
+			return dialer(loopbackProtectedCtx, dest, c.config.PrimaryEgressOutbound)
 		}, c.serverIdentity)
 	if err != nil {
 		return newError("failed to create HTTP round tripper for enrollment confirmation").Base(err).AtError()
@@ -93,7 +97,14 @@ func (c *EnrollmentConfirmationClient) init() error {
 		if err != nil {
 			return newError("failed to get instance of bootstrap enrollment confirmation config").Base(err).AtError()
 		}
-		enrollmentConfirmation, ok := enrollment.(tlsmirror.ConnectionEnrollmentConfirmation)
+
+		loopbackProtectedCtx := mirrorcommon.SetLoopbackProtectionFlagForContext(c.ctx, c.serverIdentity)
+		enrollmentInst, err := common.CreateObject(loopbackProtectedCtx, enrollment)
+		if err != nil {
+			return newError("failed to create bootstrap enrollment confirmation config").Base(err).AtError()
+		}
+
+		enrollmentConfirmation, ok := enrollmentInst.(tlsmirror.ConnectionEnrollmentConfirmation)
 		if !ok {
 			return newError("bootstrap enrollment confirmation config is not a valid ConnectionEnrollmentConfirmation")
 

transport/internet/tlsmirror/mirrorenrollment/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 
+	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/serial"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorenrollment/httpenrollmentconfirmation"
@@ -36,6 +37,10 @@ func NewEnrollmentConfirmationServer(ctx context.Context, config *Config, enroll
 		primaryIngressConnectionHandler: primaryIngressConnectionHandler,
 	}
 
+	if err = s.init(); err != nil {
+		return nil, newError("failed to initialize enrollment confirmation server").Base(err).AtError()
+	}
+
 	return s, nil
 }
 
@@ -59,21 +64,25 @@ func (s *EnrollmentConfirmationServer) HandlePrimaryIngressConnection(ctx contex
 }
 
 func (s *EnrollmentConfirmationServer) init() error {
-	for _, handler := range s.config.BootstrapEgressConfig {
+	for _, handler := range s.config.BootstrapIngressConfig {
 		bootstrapEnrollmentHandler, err := serial.GetInstanceOf(handler)
 		if err != nil {
 			return newError("failed to get instance of bootstrap enrollment handler").Base(err).AtError()
 		}
-		bootstrapEnrollmentHandlerTyped, ok := bootstrapEnrollmentHandler.(tlsmirror.ConnectionEnrollmentConfirmationServerInstanceConfigReceiver)
+		bootstrapEnrollmentHandlerObj, err := common.CreateObject(s.ctx, bootstrapEnrollmentHandler)
+		if err != nil {
+			return newError("failed to create bootstrap enrollment handler").Base(err).AtError()
+		}
+		bootstrapEnrollmentHandlerObjTyped, ok := bootstrapEnrollmentHandlerObj.(tlsmirror.ConnectionEnrollmentConfirmationServerInstanceConfigReceiver)
 		if !ok {
 			return newError("bootstrap enrollment handler is not a valid ConnectionEnrollmentConfirmationServerInstanceConfigReceiver")
 		}
 
-		bootstrapEnrollmentHandlerTyped.OnConnectionEnrollmentConfirmationServerInstanceConfigReady(
+		bootstrapEnrollmentHandlerObjTyped.OnConnectionEnrollmentConfirmationServerInstanceConfigReady(
 			tlsmirror.ConnectionEnrollmentConfirmationServerInstanceConfig{
 				EnrollmentProcessor: s.enrollmentProcessor,
 			})
-		s.bootstrapIngressConnectionHandlers = append(s.bootstrapIngressConnectionHandlers, bootstrapEnrollmentHandlerTyped)
+		s.bootstrapIngressConnectionHandlers = append(s.bootstrapIngressConnectionHandlers, bootstrapEnrollmentHandlerObjTyped)
 	}
 	return nil
 }

transport/internet/tlsmirror/server/client.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorcommon"
 
 	core "github.com/v2fly/v2ray-core/v5"
 	"github.com/v2fly/v2ray-core/v5/common"
@@ -58,6 +59,8 @@ type persistentMirrorTLSDialer struct {
 	explicitNonceCiphersuiteLookup *ciphersuiteLookuper
 
 	enrollmentConfirmationClient *mirrorenrollment.EnrollmentConfirmationClient
+
+	enrollmentServerIdentifier []byte
 }
 
 func (d *persistentMirrorTLSDialer) init(ctx context.Context, config *Config) error {
@@ -147,6 +150,7 @@ func (d *persistentMirrorTLSDialer) init(ctx context.Context, config *Config) er
 		if err != nil {
 			return newError("failed to derive enrollment server identifier").Base(err).AtError()
 		}
+		d.enrollmentServerIdentifier = enrollmentServerIdentifier
 		d.enrollmentConfirmationClient, err = mirrorenrollment.NewEnrollmentConfirmationClient(d.ctx, d.config.ConnectionEnrolment, enrollmentServerIdentifier)
 		if err != nil {
 			return newError("failed to create enrollment confirmation client").Base(err).AtError()
@@ -259,6 +263,12 @@ func (d *persistentMirrorTLSDialer) handleIncomingReadyConnection(conn internet.
 func (d *persistentMirrorTLSDialer) Dial(ctx context.Context,
 	dest net.Destination, settings *internet.MemoryStreamConfig,
 ) (internet.Connection, error) {
+	if d.enrollmentServerIdentifier != nil && len(d.enrollmentServerIdentifier) > 0 {
+		if mirrorcommon.IsLoopbackProtectionEnabled(ctx, d.enrollmentServerIdentifier) {
+			return nil, newError("loopback protection: refusing to dial to self")
+		}
+	}
+
 	var recvConn net.Conn
 	select {
 	case conn := <-d.incomingConnections:

dyhkwong/v2ray-core

transport/internet/tlsmirror/mirrorenrollment/client.go
@@ -7,10 +7,12 @@ import (
 	"context"
 	"net"
 
+	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/environment"
 	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
 	v2net "github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror/mirrorcommon"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror/mirrorenrollment/httpenrollmentconfirmation"
 )
 
@@ -80,7 +82,9 @@ func (c *EnrollmentConfirmationClient) init() error {
 				return nil, newError("failed to parse destination address").Base(err).AtError()
 			}
 			dest.Network = v2net.Network_TCP
-			return dialer(c.ctx, dest, c.config.PrimaryEgressOutbound)
+
+			loopbackProtectedCtx := mirrorcommon.SetLoopbackProtectionFlagForContext(c.ctx, c.serverIdentity)
+			return dialer(loopbackProtectedCtx, dest, c.config.PrimaryEgressOutbound)
 		}, c.serverIdentity)
 	if err != nil {
 		return newError("failed to create HTTP round tripper for enrollment confirmation").Base(err).AtError()
@@ -95,7 +99,14 @@ func (c *EnrollmentConfirmationClient) init() error {
 		if err != nil {
 			return newError("failed to get instance of bootstrap enrollment confirmation config").Base(err).AtError()
 		}
-		enrollmentConfirmation, ok := enrollment.(tlsmirror.ConnectionEnrollmentConfirmation)
+
+		loopbackProtectedCtx := mirrorcommon.SetLoopbackProtectionFlagForContext(c.ctx, c.serverIdentity)
+		enrollmentInst, err := common.CreateObject(loopbackProtectedCtx, enrollment)
+		if err != nil {
+			return newError("failed to create bootstrap enrollment confirmation config").Base(err).AtError()
+		}
+
+		enrollmentConfirmation, ok := enrollmentInst.(tlsmirror.ConnectionEnrollmentConfirmation)
 		if !ok {
 			return newError("bootstrap enrollment confirmation config is not a valid ConnectionEnrollmentConfirmation")
 

transport/internet/tlsmirror/mirrorenrollment/server.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"net"
 
+	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror/mirrorenrollment/httpenrollmentconfirmation"
 )
@@ -38,6 +39,10 @@ func NewEnrollmentConfirmationServer(ctx context.Context, config *Config, enroll
 		primaryIngressConnectionHandler: primaryIngressConnectionHandler,
 	}
 
+	if err = s.init(); err != nil {
+		return nil, newError("failed to initialize enrollment confirmation server").Base(err).AtError()
+	}
+
 	return s, nil
 }
 
@@ -61,21 +66,25 @@ func (s *EnrollmentConfirmationServer) HandlePrimaryIngressConnection(ctx contex
 }
 
 func (s *EnrollmentConfirmationServer) init() error {
-	for _, handler := range s.config.BootstrapEgressConfig {
+	for _, handler := range s.config.BootstrapIngressConfig {
 		bootstrapEnrollmentHandler, err := handler.GetInstance()
 		if err != nil {
 			return newError("failed to get instance of bootstrap enrollment handler").Base(err).AtError()
 		}
-		bootstrapEnrollmentHandlerTyped, ok := bootstrapEnrollmentHandler.(tlsmirror.ConnectionEnrollmentConfirmationServerInstanceConfigReceiver)
+		bootstrapEnrollmentHandlerObj, err := common.CreateObject(s.ctx, bootstrapEnrollmentHandler)
+		if err != nil {
+			return newError("failed to create bootstrap enrollment handler").Base(err).AtError()
+		}
+		bootstrapEnrollmentHandlerObjTyped, ok := bootstrapEnrollmentHandlerObj.(tlsmirror.ConnectionEnrollmentConfirmationServerInstanceConfigReceiver)
 		if !ok {
 			return newError("bootstrap enrollment handler is not a valid ConnectionEnrollmentConfirmationServerInstanceConfigReceiver")
 		}
 
-		bootstrapEnrollmentHandlerTyped.OnConnectionEnrollmentConfirmationServerInstanceConfigReady(
+		bootstrapEnrollmentHandlerObjTyped.OnConnectionEnrollmentConfirmationServerInstanceConfigReady(
 			tlsmirror.ConnectionEnrollmentConfirmationServerInstanceConfig{
 				EnrollmentProcessor: s.enrollmentProcessor,
 			})
-		s.bootstrapIngressConnectionHandlers = append(s.bootstrapIngressConnectionHandlers, bootstrapEnrollmentHandlerTyped)
+		s.bootstrapIngressConnectionHandlers = append(s.bootstrapIngressConnectionHandlers, bootstrapEnrollmentHandlerObjTyped)
 	}
 	return nil
 }

transport/internet/tlsmirror/server/client.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror/mirrorcommon"
 
 	core "github.com/v2fly/v2ray-core/v4"
 	"github.com/v2fly/v2ray-core/v4/common"
@@ -61,6 +62,8 @@ type persistentMirrorTLSDialer struct {
 	explicitNonceCiphersuiteLookup *ciphersuiteLookuper
 
 	enrollmentConfirmationClient *mirrorenrollment.EnrollmentConfirmationClient
+
+	enrollmentServerIdentifier []byte
 }
 
 func (d *persistentMirrorTLSDialer) init(ctx context.Context, config *Config) error {
@@ -150,6 +153,7 @@ func (d *persistentMirrorTLSDialer) init(ctx context.Context, config *Config) er
 		if err != nil {
 			return newError("failed to derive enrollment server identifier").Base(err).AtError()
 		}
+		d.enrollmentServerIdentifier = enrollmentServerIdentifier
 		d.enrollmentConfirmationClient, err = mirrorenrollment.NewEnrollmentConfirmationClient(d.ctx, d.config.ConnectionEnrolment, enrollmentServerIdentifier)
 		if err != nil {
 			return newError("failed to create enrollment confirmation client").Base(err).AtError()
@@ -262,6 +266,12 @@ func (d *persistentMirrorTLSDialer) handleIncomingReadyConnection(conn internet.
 func (d *persistentMirrorTLSDialer) Dial(ctx context.Context,
 	dest net.Destination, settings *internet.MemoryStreamConfig,
 ) (internet.Connection, error) {
+	if d.enrollmentServerIdentifier != nil && len(d.enrollmentServerIdentifier) > 0 {
+		if mirrorcommon.IsLoopbackProtectionEnabled(ctx, d.enrollmentServerIdentifier) {
+			return nil, newError("loopback protection: refusing to dial to self")
+		}
+	}
+
 	var recvConn net.Conn
 	select {
 	case conn := <-d.incomingConnections: