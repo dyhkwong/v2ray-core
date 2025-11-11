xiaokangwang/v2ray-core-1

Add request transport based enrollment receiver

transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation/client.go
@@ -1,0 +1,136 @@
+package roundtripperenrollmentconfirmation
+
+import (
+	"context"
+	csrand "crypto/rand"
+	"net"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
+	v2net "github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/serial"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/request"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/security"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror"
+)
+
+func NewClient(ctx context.Context, config *ClientConfig) (*Client, error) {
+	if ctx == nil {
+		return nil, newError("context cannot be nil")
+	}
+
+	if config == nil {
+		return nil, newError("config cannot be nil")
+	}
+
+	rttClientI, err := serial.GetInstanceOf(config.RoundTripperClient)
+	if err != nil {
+		return nil, newError("failed to get instance of RoundTripperClient").Base(err)
+	}
+
+	rttClient, ok := rttClientI.(request.RoundTripperClient)
+	if !ok {
+		return nil, newError("RoundTripperClient is not a valid request.RoundTripperClient")
+	}
+
+	clientTemporaryIdentifier := make([]byte, 16)
+	if _, err := csrand.Read(clientTemporaryIdentifier); err != nil {
+		return nil, newError("failed to generate client temporary identifier").Base(err)
+	}
+
+	c := &Client{
+		ctx:                       ctx,
+		config:                    config,
+		rttClient:                 rttClient,
+		clientTemporaryIdentifier: clientTemporaryIdentifier,
+	}
+
+	rttClient.OnTransportClientAssemblyReady(c)
+
+	return c, nil
+}
+
+type Client struct {
+	config    *ClientConfig
+	rttClient request.RoundTripperClient
+
+	clientTemporaryIdentifier []byte
+
+	ctx context.Context
+
+	defaultOutboundTag string
+}
+
+func (c *Client) OnConnectionEnrollmentConfirmationClientInstanceConfigReady(config tlsmirror.ConnectionEnrollmentConfirmationClientInstanceConfig) {
+	c.defaultOutboundTag = config.DefaultOutboundTag
+}
+
+func (c *Client) Dial(ctx context.Context) (net.Conn, error) {
+	transportEnvironment := envctx.EnvironmentFromContext(c.ctx).(environment.TransportEnvironment)
+	dialer := transportEnvironment.OutboundDialer()
+	if dialer == nil {
+		return nil, newError("no outbound dialer available in transport environment")
+	}
+	dest, err := v2net.ParseDestination(c.config.Dest)
+	if err != nil {
+		return nil, newError("failed to parse destination address").Base(err).AtError()
+	}
+	dest.Network = v2net.Network_TCP
+	conn, err := dialer(c.ctx, dest, c.config.OutboundTag)
+	if err != nil {
+		return nil, newError("failed to dial to destination").Base(err).AtError()
+	}
+	if c.config.SecurityConfig != nil {
+		securityEngine, err := common.CreateObject(c.ctx, c.config.SecurityConfig)
+		if err != nil {
+			return nil, newError("unable to create security engine from security settings").Base(err)
+		}
+		securityEngineTyped, ok := securityEngine.(security.Engine)
+		if !ok {
+			return nil, newError("type assertion error when create security engine from security settings")
+		}
+		conn, err = securityEngineTyped.Client(conn, security.OptionWithDestination{Dest: dest})
+		if err != nil {
+			return nil, newError("unable to create security protocol client from security engine").Base(err)
+		}
+	}
+	return conn, nil
+}
+
+func (c *Client) Tripper() request.Tripper {
+	return c.rttClient
+}
+
+func (c *Client) AutoImplDialer() request.Dialer {
+	return c
+}
+
+func (c *Client) VerifyConnectionEnrollment(req *tlsmirror.EnrollmentConfirmationReq) (*tlsmirror.EnrollmentConfirmationResp, error) {
+	connectionTag := append(req.ServerIdentifier, c.clientTemporaryIdentifier...)
+	wrappedData, err := proto.Marshal(req)
+	if err != nil {
+		return nil, newError("failed to marshal enrollment confirmation request").Base(err)
+	}
+	wreq := request.Request{
+		Data:          wrappedData,
+		ConnectionTag: connectionTag,
+	}
+	resp, err := c.rttClient.RoundTrip(c.ctx, wreq)
+	if err != nil {
+		return nil, newError("failed to perform round trip").Base(err)
+	}
+	confirmationResp := &tlsmirror.EnrollmentConfirmationResp{}
+	if err := proto.Unmarshal(resp.Data, confirmationResp); err != nil {
+		return nil, newError("failed to unmarshal enrollment confirmation response").Base(err)
+	}
+	return confirmationResp, nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ClientConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewClient(ctx, config.(*ClientConfig))
+	}))
+}

transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation/errors.generated.go
@@ -1,0 +1,9 @@
+package roundtripperenrollmentconfirmation
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation/server.go
@@ -1,0 +1,82 @@
+package roundtripperenrollmentconfirmation
+
+import (
+	"context"
+	"net"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
+	v2net "github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/request"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror"
+)
+
+func NewServer(ctx context.Context, config *ServerConfig) *Server {
+	s := &Server{
+		ctx:    ctx,
+		config: config,
+	}
+	return s
+}
+
+type Server struct {
+	config              *ServerConfig
+	ctx                 context.Context
+	enrollmentProcessor tlsmirror.ConnectionEnrollmentConfirmationProcessor
+	rttServer           request.RoundTripperServer
+}
+
+func (s *Server) Listen(ctx context.Context) (v2net.Listener, error) {
+	transportEnvironment := envctx.EnvironmentFromContext(s.ctx).(environment.TransportEnvironment)
+	listener := transportEnvironment.Listener()
+	addr, err := v2net.ParseDestination(s.config.Listen)
+	if err != nil {
+		panic(newError("invalid listen address " + s.config.Listen).Base(err).AtError())
+	}
+	netaddr := &net.TCPAddr{IP: addr.Address.IP(), Port: int(addr.Port)}
+	l, err := listener.Listen(s.ctx, netaddr, nil)
+	if err != nil {
+		panic(newError("failed to listen on " + s.config.Listen).Base(err).AtError())
+	}
+	return l, nil
+}
+
+func (s *Server) OnRoundTrip(ctx context.Context, req request.Request, opts ...request.RoundTripperOption) (resp request.Response, err error) {
+	enrollmentReq := &tlsmirror.EnrollmentConfirmationReq{}
+	err = proto.Unmarshal(req.Data, enrollmentReq)
+	if err != nil {
+		return request.Response{}, newError("failed to unmarshal enrollment confirmation request").Base(err).AtError()
+	}
+	enrollmentResp, err := s.enrollmentProcessor.VerifyConnectionEnrollment(enrollmentReq)
+	if err != nil {
+		return request.Response{}, newError("failed to process enrollment confirmation request").Base(err).AtError()
+	}
+	respData, err := proto.Marshal(enrollmentResp)
+	if err != nil {
+		return request.Response{}, newError("failed to marshal enrollment confirmation response").Base(err).AtError()
+	}
+	return request.Response{
+		Data: respData,
+	}, nil
+}
+
+func (s *Server) TripperReceiver() request.TripperReceiver {
+	return s
+}
+
+func (s *Server) SessionReceiver() request.SessionReceiver {
+	return nil
+}
+
+func (s *Server) AutoImplListener() request.Listener {
+	return s
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ServerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewServer(ctx, config.(*ServerConfig)), nil
+	}))
+}

dyhkwong/v2ray-core

transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation/client.go
@@ -1,0 +1,138 @@
+//go:build !confonly
+// +build !confonly
+
+package roundtripperenrollmentconfirmation
+
+import (
+	"context"
+	csrand "crypto/rand"
+	"net"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
+	v2net "github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/request"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/security"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror"
+)
+
+func NewClient(ctx context.Context, config *ClientConfig) (*Client, error) {
+	if ctx == nil {
+		return nil, newError("context cannot be nil")
+	}
+
+	if config == nil {
+		return nil, newError("config cannot be nil")
+	}
+
+	rttClientI, err := config.RoundTripperClient.GetInstance()
+	if err != nil {
+		return nil, newError("failed to get instance of RoundTripperClient").Base(err)
+	}
+
+	rttClient, ok := rttClientI.(request.RoundTripperClient)
+	if !ok {
+		return nil, newError("RoundTripperClient is not a valid request.RoundTripperClient")
+	}
+
+	clientTemporaryIdentifier := make([]byte, 16)
+	if _, err := csrand.Read(clientTemporaryIdentifier); err != nil {
+		return nil, newError("failed to generate client temporary identifier").Base(err)
+	}
+
+	c := &Client{
+		ctx:                       ctx,
+		config:                    config,
+		rttClient:                 rttClient,
+		clientTemporaryIdentifier: clientTemporaryIdentifier,
+	}
+
+	rttClient.OnTransportClientAssemblyReady(c)
+
+	return c, nil
+}
+
+type Client struct {
+	config    *ClientConfig
+	rttClient request.RoundTripperClient
+
+	clientTemporaryIdentifier []byte
+
+	ctx context.Context
+
+	defaultOutboundTag string
+}
+
+func (c *Client) OnConnectionEnrollmentConfirmationClientInstanceConfigReady(config tlsmirror.ConnectionEnrollmentConfirmationClientInstanceConfig) {
+	c.defaultOutboundTag = config.DefaultOutboundTag
+}
+
+func (c *Client) Dial(ctx context.Context) (net.Conn, error) {
+	transportEnvironment := envctx.EnvironmentFromContext(c.ctx).(environment.TransportEnvironment)
+	dialer := transportEnvironment.OutboundDialer()
+	if dialer == nil {
+		return nil, newError("no outbound dialer available in transport environment")
+	}
+	dest, err := v2net.ParseDestination(c.config.Dest)
+	if err != nil {
+		return nil, newError("failed to parse destination address").Base(err).AtError()
+	}
+	dest.Network = v2net.Network_TCP
+	conn, err := dialer(c.ctx, dest, c.config.OutboundTag)
+	if err != nil {
+		return nil, newError("failed to dial to destination").Base(err).AtError()
+	}
+	if c.config.SecurityConfig != nil {
+		securityEngine, err := common.CreateObject(c.ctx, c.config.SecurityConfig)
+		if err != nil {
+			return nil, newError("unable to create security engine from security settings").Base(err)
+		}
+		securityEngineTyped, ok := securityEngine.(security.Engine)
+		if !ok {
+			return nil, newError("type assertion error when create security engine from security settings")
+		}
+		conn, err = securityEngineTyped.Client(conn, security.OptionWithDestination{Dest: dest})
+		if err != nil {
+			return nil, newError("unable to create security protocol client from security engine").Base(err)
+		}
+	}
+	return conn, nil
+}
+
+func (c *Client) Tripper() request.Tripper {
+	return c.rttClient
+}
+
+func (c *Client) AutoImplDialer() request.Dialer {
+	return c
+}
+
+func (c *Client) VerifyConnectionEnrollment(req *tlsmirror.EnrollmentConfirmationReq) (*tlsmirror.EnrollmentConfirmationResp, error) {
+	connectionTag := append(req.ServerIdentifier, c.clientTemporaryIdentifier...)
+	wrappedData, err := proto.Marshal(req)
+	if err != nil {
+		return nil, newError("failed to marshal enrollment confirmation request").Base(err)
+	}
+	wreq := request.Request{
+		Data:          wrappedData,
+		ConnectionTag: connectionTag,
+	}
+	resp, err := c.rttClient.RoundTrip(c.ctx, wreq)
+	if err != nil {
+		return nil, newError("failed to perform round trip").Base(err)
+	}
+	confirmationResp := &tlsmirror.EnrollmentConfirmationResp{}
+	if err := proto.Unmarshal(resp.Data, confirmationResp); err != nil {
+		return nil, newError("failed to unmarshal enrollment confirmation response").Base(err)
+	}
+	return confirmationResp, nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ClientConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewClient(ctx, config.(*ClientConfig))
+	}))
+}

transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation/errors.generated.go
@@ -1,0 +1,9 @@
+package roundtripperenrollmentconfirmation
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation/server.go
@@ -1,0 +1,85 @@
+//go:build !confonly
+// +build !confonly
+
+package roundtripperenrollmentconfirmation
+
+import (
+	"context"
+	"net"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
+	v2net "github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/request"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror"
+)
+
+func NewServer(ctx context.Context, config *ServerConfig) *Server {
+	s := &Server{
+		ctx:    ctx,
+		config: config,
+	}
+	return s
+}
+
+type Server struct {
+	config              *ServerConfig
+	ctx                 context.Context
+	enrollmentProcessor tlsmirror.ConnectionEnrollmentConfirmationProcessor
+	rttServer           request.RoundTripperServer
+}
+
+func (s *Server) Listen(ctx context.Context) (v2net.Listener, error) {
+	transportEnvironment := envctx.EnvironmentFromContext(s.ctx).(environment.TransportEnvironment)
+	listener := transportEnvironment.Listener()
+	addr, err := v2net.ParseDestination(s.config.Listen)
+	if err != nil {
+		panic(newError("invalid listen address " + s.config.Listen).Base(err).AtError())
+	}
+	netaddr := &net.TCPAddr{IP: addr.Address.IP(), Port: int(addr.Port)}
+	l, err := listener.Listen(s.ctx, netaddr, nil)
+	if err != nil {
+		panic(newError("failed to listen on " + s.config.Listen).Base(err).AtError())
+	}
+	return l, nil
+}
+
+func (s *Server) OnRoundTrip(ctx context.Context, req request.Request, opts ...request.RoundTripperOption) (resp request.Response, err error) {
+	enrollmentReq := &tlsmirror.EnrollmentConfirmationReq{}
+	err = proto.Unmarshal(req.Data, enrollmentReq)
+	if err != nil {
+		return request.Response{}, newError("failed to unmarshal enrollment confirmation request").Base(err).AtError()
+	}
+	enrollmentResp, err := s.enrollmentProcessor.VerifyConnectionEnrollment(enrollmentReq)
+	if err != nil {
+		return request.Response{}, newError("failed to process enrollment confirmation request").Base(err).AtError()
+	}
+	respData, err := proto.Marshal(enrollmentResp)
+	if err != nil {
+		return request.Response{}, newError("failed to marshal enrollment confirmation response").Base(err).AtError()
+	}
+	return request.Response{
+		Data: respData,
+	}, nil
+}
+
+func (s *Server) TripperReceiver() request.TripperReceiver {
+	return s
+}
+
+func (s *Server) SessionReceiver() request.SessionReceiver {
+	return nil
+}
+
+func (s *Server) AutoImplListener() request.Listener {
+	return s
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ServerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewServer(ctx, config.(*ServerConfig)), nil
+	}))
+}