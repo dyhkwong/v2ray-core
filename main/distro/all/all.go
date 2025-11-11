xiaokangwang/v2ray-core-1

enable roundtripper enrollment for tlsmirror

main/distro/all/all.go
@@ -87,6 +87,7 @@ import (
 
 	_ "github.com/v2fly/v2ray-core/v5/transport/internet/hysteria2"
 
+	_ "github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation"
 	_ "github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/server"
 
 	// Transport headers

dyhkwong/v2ray-core

main/distro/all/all.go
@@ -85,6 +85,7 @@ import (
 
 	_ "github.com/v2fly/v2ray-core/v4/transport/internet/hysteria2"
 
+	_ "github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror/mirrorenrollment/roundtripperenrollmentconfirmation"
 	_ "github.com/v2fly/v2ray-core/v4/transport/internet/tlsmirror/server"
 
 	// Transport headers