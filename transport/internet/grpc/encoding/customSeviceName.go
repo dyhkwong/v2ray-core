79b5f1159f090cd2c5dc0f6614e0595197f96fe6
Jim Han <50871214+JimhHan@users.noreply.github.com>
2025-11-23 08:06:26 +0800

    Import gRPC transport multiMode client

--- transport/internet/grpc/encoding/customSeviceName.go
+++ transport/internet/grpc/encoding/customSeviceName.go
@@ -2,6 +2,8 @@ package encoding
 
 import (
 	"context"
+	"net/url"
+	"strings"
 
 	"google.golang.org/grpc"
 )
@@ -32,9 +34,57 @@ func (c *gunServiceClient) TunCustomName(ctx context.Context, name string, opts 
 	return x, nil
 }
 
+func (c *gunServiceClient) TunCustomNameX(ctx context.Context, name string, opts ...grpc.CallOption) (grpc.BidiStreamingClient[Hunk, Hunk], error) {
+	var path string
+	if !strings.HasPrefix(name, "/") {
+		path = "/" + url.PathEscape(name) + "/Tun"
+	} else {
+		path = name
+	}
+	stream, err := c.cc.NewStream(ctx, &grpc.StreamDesc{
+		ServerStreams: true,
+		ClientStreams: true,
+	}, path, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return &grpc.GenericClientStream[Hunk, Hunk]{
+		ClientStream: stream,
+	}, nil
+}
+
+func (c *gunMultiServiceClient) TunCustomName(ctx context.Context, name string, opts ...grpc.CallOption) (grpc.BidiStreamingClient[MultiHunk, MultiHunk], error) {
+	var path string
+	if !strings.HasPrefix(name, "/") {
+		path = "/" + url.PathEscape(name) + "/TunMulti"
+	} else {
+		path = name
+	}
+	stream, err := c.cc.NewStream(ctx, &grpc.StreamDesc{
+		ServerStreams: true,
+		ClientStreams: true,
+	}, path, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return &grpc.GenericClientStream[MultiHunk, MultiHunk]{
+		ClientStream: stream,
+	}, nil
+}
+
 type GunServiceClientX interface {
 	TunCustomName(ctx context.Context, name string, opts ...grpc.CallOption) (grpc.BidiStreamingClient[Hunk, Hunk], error)
 	Tun(ctx context.Context, opts ...grpc.CallOption) (grpc.BidiStreamingClient[Hunk, Hunk], error)
+}
+
+type GunServiceClientXWithTunCustomNameX interface {
+	GunServiceClientX
+	TunCustomNameX(ctx context.Context, name string, opts ...grpc.CallOption) (grpc.BidiStreamingClient[Hunk, Hunk], error)
+}
+
+type GunMultiServiceClientX interface {
+	TunCustomName(ctx context.Context, name string, opts ...grpc.CallOption) (grpc.BidiStreamingClient[MultiHunk, MultiHunk], error)
+	Tun(ctx context.Context, opts ...grpc.CallOption) (grpc.BidiStreamingClient[MultiHunk, MultiHunk], error)
 }
 
 func RegisterGunServiceServerX(s *grpc.Server, srv GunServiceServer, name string) {
