ef0cf9987a3501c93231dd49c2c92760d4a96164
Jim Han <50871214+JimhHan@users.noreply.github.com>
2025-11-23 08:06:26 +0800

    Import gRPC transport multiMode client

--- transport/internet/grpc/encoding/stream.pb.go
+++ transport/internet/grpc/encoding/stream.pb.go
@@ -59,16 +59,64 @@ func (x *Hunk) GetData() []byte {
 	return nil
 }
 
+type MultiHunk struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Data          [][]byte               `protobuf:"bytes,1,rep,name=data,proto3" json:"data,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *MultiHunk) Reset() {
+	*x = MultiHunk{}
+	mi := &file_transport_internet_grpc_encoding_stream_proto_msgTypes[1]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *MultiHunk) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*MultiHunk) ProtoMessage() {}
+
+func (x *MultiHunk) ProtoReflect() protoreflect.Message {
+	mi := &file_transport_internet_grpc_encoding_stream_proto_msgTypes[1]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use MultiHunk.ProtoReflect.Descriptor instead.
+func (*MultiHunk) Descriptor() ([]byte, []int) {
+	return file_transport_internet_grpc_encoding_stream_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *MultiHunk) GetData() [][]byte {
+	if x != nil {
+		return x.Data
+	}
+	return nil
+}
+
 var File_transport_internet_grpc_encoding_stream_proto protoreflect.FileDescriptor
 
 const file_transport_internet_grpc_encoding_stream_proto_rawDesc = "" +
 	"\n" +
 	"-transport/internet/grpc/encoding/stream.proto\x12+v2ray.core.transport.internet.grpc.encoding\"\x1a\n" +
 	"\x04Hunk\x12\x12\n" +
-	"\x04data\x18\x01 \x01(\fR\x04data2}\n" +
+	"\x04data\x18\x01 \x01(\fR\x04data\"\x1f\n" +
+	"\tMultiHunk\x12\x12\n" +
+	"\x04data\x18\x01 \x03(\fR\x04data2}\n" +
 	"\n" +
 	"GunService\x12o\n" +
-	"\x03Tun\x121.v2ray.core.transport.internet.grpc.encoding.Hunk\x1a1.v2ray.core.transport.internet.grpc.encoding.Hunk(\x010\x01B\xa2\x01\n" +
+	"\x03Tun\x121.v2ray.core.transport.internet.grpc.encoding.Hunk\x1a1.v2ray.core.transport.internet.grpc.encoding.Hunk(\x010\x012\x8c\x01\n" +
+	"\x0fGunMultiService\x12y\n" +
+	"\x03Tun\x126.v2ray.core.transport.internet.grpc.encoding.MultiHunk\x1a6.v2ray.core.transport.internet.grpc.encoding.MultiHunk(\x010\x01B\xa2\x01\n" +
 	"/com.v2ray.core.transport.internet.grpc.encodingP\x01Z?github.com/v2fly/v2ray-core/v5/transport/internet/grpc/encoding\xaa\x02+V2Ray.Core.Transport.Internet.Grpc.Encodingb\x06proto3"
 
 var (
@@ -83,15 +131,18 @@ func file_transport_internet_grpc_encoding_stream_proto_rawDescGZIP() []byte {
 	return file_transport_internet_grpc_encoding_stream_proto_rawDescData
 }
 
-var file_transport_internet_grpc_encoding_stream_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_transport_internet_grpc_encoding_stream_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_transport_internet_grpc_encoding_stream_proto_goTypes = []any{
-	(*Hunk)(nil), // 0: v2ray.core.transport.internet.grpc.encoding.Hunk
+	(*Hunk)(nil),      // 0: v2ray.core.transport.internet.grpc.encoding.Hunk
+	(*MultiHunk)(nil), // 1: v2ray.core.transport.internet.grpc.encoding.MultiHunk
 }
 var file_transport_internet_grpc_encoding_stream_proto_depIdxs = []int32{
 	0, // 0: v2ray.core.transport.internet.grpc.encoding.GunService.Tun:input_type -> v2ray.core.transport.internet.grpc.encoding.Hunk
-	0, // 1: v2ray.core.transport.internet.grpc.encoding.GunService.Tun:output_type -> v2ray.core.transport.internet.grpc.encoding.Hunk
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	1, // 1: v2ray.core.transport.internet.grpc.encoding.GunMultiService.Tun:input_type -> v2ray.core.transport.internet.grpc.encoding.MultiHunk
+	0, // 2: v2ray.core.transport.internet.grpc.encoding.GunService.Tun:output_type -> v2ray.core.transport.internet.grpc.encoding.Hunk
+	1, // 3: v2ray.core.transport.internet.grpc.encoding.GunMultiService.Tun:output_type -> v2ray.core.transport.internet.grpc.encoding.MultiHunk
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -108,9 +159,9 @@ func file_transport_internet_grpc_encoding_stream_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_transport_internet_grpc_encoding_stream_proto_rawDesc), len(file_transport_internet_grpc_encoding_stream_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
-			NumServices:   1,
+			NumServices:   2,
 		},
 		GoTypes:           file_transport_internet_grpc_encoding_stream_proto_goTypes,
 		DependencyIndexes: file_transport_internet_grpc_encoding_stream_proto_depIdxs,
