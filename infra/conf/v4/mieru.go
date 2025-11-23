e65b53a9129923027cd77ba5a0564579396fc1fa
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-11-23 22:46:05 +0800

    Import mieru outbound

---
+++ infra/conf/v4/mieru.go
@@ -0,0 +1,42 @@
+package v4
+
+import (
+	"github.com/golang/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v5/infra/conf/cfgcommon"
+	"github.com/v2fly/v2ray-core/v5/proxy/mieru"
+)
+
+type MieruClientConfig struct {
+	Address       *cfgcommon.Address `json:"address"`
+	Port          uint16             `json:"port"`
+	PortRange     []string           `json:"portRange"`
+	Username      string             `json:"username"`
+	Password      string             `json:"password"`
+	Protocol      string             `json:"protocol"`
+	Multiplexing  string             `json:"multiplexing"`
+	HandshakeMode string             `json:"handshakeMode"`
+}
+
+type MieruPortBindingConfig struct {
+	Port      uint16 `json:"port"`
+	PortRange string `json:"portRange"`
+	Protocol  string `json:"protocol"`
+}
+
+func (c *MieruClientConfig) Build() (proto.Message, error) {
+	if c.Address == nil {
+		return nil, newError("missing server address")
+	}
+	config := &mieru.ClientConfig{
+		Address:       c.Address.Build(),
+		Port:          uint32(c.Port),
+		PortRange:     c.PortRange,
+		Username:      c.Username,
+		Password:      c.Password,
+		Protocol:      c.Protocol,
+		Multiplexing:  c.Multiplexing,
+		HandshakeMode: c.HandshakeMode,
+	}
+	return config, nil
+}
