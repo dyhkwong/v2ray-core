c77c6d891ca48862b75e67a336897931600c7b70
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-11-30 21:29:46 +0800

    Fix outbound domainStratrgy for some transports

--- proxy/freedom/freedom.go
+++ proxy/freedom/freedom.go
@@ -43,6 +43,7 @@ type Handler struct {
 	policyManager policy.Manager
 	dns           dns.Client
 	config        *Config
+	resolver      func(ctx context.Context, domain string) net.Address
 }
 
 // Init initializes the Handler with necessary parameters.
@@ -122,10 +123,16 @@ func (h *Handler) Process(ctx context.Context, link *transport.Link, dialer inte
 			destination.Network = net.Network_UDP
 		}
 	}
-	if h.config.useIP() {
-		outbound.Resolver = func(ctx context.Context, domain string) net.Address {
+	switch {
+	case outbound.Resolver != nil:
+		h.resolver = outbound.Resolver
+	case outbound.TargetResolver != nil:
+		h.resolver = outbound.TargetResolver
+	case h.config.useIP():
+		h.resolver = func(ctx context.Context, domain string) net.Address {
 			return h.resolveIP(ctx, domain, dialer.Address())
 		}
+		outbound.Resolver = h.resolver
 	}
 	newError("opening connection to ", destination).WriteToLog(session.ExportIDToError(ctx))
 
@@ -297,8 +304,8 @@ func (w *PacketWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
 			dest = w.dest
 		}
 		originalDest := dest
-		if w.handler.config.useIP() && dest.Address.Family().IsDomain() {
-			ip := w.handler.resolveIP(w.ctx, dest.Address.Domain(), nil)
+		if w.handler.resolver != nil && dest.Address.Family().IsDomain() {
+			ip := w.handler.resolver(w.ctx, dest.Address.Domain())
 			if ip != nil {
 				dest.Address = ip
 			} else {
