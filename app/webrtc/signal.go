85437a3ff6d1207f9bfa530fbce5134957d847d3
Xiaokang Wang (Shelikhoo) <xiaokangwang@outlook.com>
2026-04-06 19:43:56 +0100

    Add WebRTC Tunnel Support in V2Ray
    * add webrtc tunnel support in v2ray
    
    * add webrtc tunnel support in v2ray
    
    * add webrtc tunnel support in v2ray
    
    * add webrtc tunnel support in v2ray(fix lint)

---
+++ app/webrtc/signal.go
@@ -0,0 +1,127 @@
+//go:build !confonly
+// +build !confonly
+
+package webrtc
+
+import (
+	"context"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
+	v2net "github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/serial"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/request"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/security"
+)
+
+type signaler struct {
+	ctx         context.Context
+	rttClient   request.RoundTripperClient
+	dest        string
+	outboundTag string
+	securityCfg *serial.TypedMessage
+}
+
+func newSignaler(ctx context.Context, roundTripperCfg, securityCfg *serial.TypedMessage, dest, outboundTag string) (*signaler, error) {
+	if roundTripperCfg == nil {
+		return nil, newError("missing round_tripper_client config")
+	}
+
+	instance, err := roundTripperCfg.GetInstance()
+	if err != nil {
+		return nil, newError("failed to get RoundTripperClient config").Base(err)
+	}
+
+	object, err := common.CreateObject(ctx, instance)
+	if err != nil {
+		return nil, newError("failed to create RoundTripperClient").Base(err)
+	}
+
+	rttClient, ok := object.(request.RoundTripperClient)
+	if !ok {
+		return nil, newError("configured object is not a request.RoundTripperClient")
+	}
+
+	s := &signaler{
+		ctx:         ctx,
+		rttClient:   rttClient,
+		dest:        dest,
+		outboundTag: outboundTag,
+		securityCfg: securityCfg,
+	}
+	rttClient.OnTransportClientAssemblyReady(s)
+	return s, nil
+}
+
+func (s *signaler) Tripper() request.Tripper {
+	return s.rttClient
+}
+
+func (s *signaler) AutoImplDialer() request.Dialer {
+	return s
+}
+
+func (s *signaler) Dial(ctx context.Context) (v2net.Conn, error) {
+	if ctx == nil {
+		ctx = s.ctx
+	}
+
+	instanceNetwork := envctx.EnvironmentFromContext(ctx).(environment.InstanceNetworkCapabilitySet)
+	dialer := instanceNetwork.OutboundDialer()
+	if dialer == nil {
+		return nil, newError("no outbound dialer available in environment")
+	}
+
+	dest, err := v2net.ParseDestination(s.dest)
+	if err != nil {
+		return nil, newError("failed to parse signaling destination").Base(err)
+	}
+	dest.Network = v2net.Network_TCP
+
+	conn, err := dialer(ctx, dest, s.outboundTag)
+	if err != nil {
+		return nil, newError("failed to dial signaling destination").Base(err)
+	}
+
+	if s.securityCfg == nil {
+		return conn, nil
+	}
+
+	securityConfig, err := s.securityCfg.GetInstance()
+	if err != nil {
+		_ = conn.Close()
+		return nil, newError("failed to decode security config").Base(err)
+	}
+
+	securityObject, err := common.CreateObject(s.ctx, securityConfig)
+	if err != nil {
+		_ = conn.Close()
+		return nil, newError("failed to create security engine").Base(err)
+	}
+
+	engine, ok := securityObject.(security.Engine)
+	if !ok {
+		_ = conn.Close()
+		return nil, newError("configured security object is not a security.Engine")
+	}
+
+	securedConn, err := engine.Client(conn, security.OptionWithDestination{Dest: dest})
+	if err != nil {
+		_ = conn.Close()
+		return nil, newError("failed to secure signaling connection").Base(err)
+	}
+
+	return securedConn, nil
+}
+
+func (s *signaler) RoundTrip(ctx context.Context, routingTag, payload []byte) ([]byte, error) {
+	resp, err := s.rttClient.RoundTrip(ctx, request.Request{
+		ConnectionTag: routingTag,
+		Data:          payload,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Data, nil
+}
--- go.mod
+++ go.mod
@@ -17,8 +17,13 @@ require (
 	github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40
 	github.com/miekg/dns v1.1.72
 	github.com/mustafaturan/bus v1.0.2
-	github.com/pion/dtls/v3 v3.0.11
+	github.com/pion/dtls/v3 v3.1.2
+	github.com/pion/ice/v4 v4.2.2
+	github.com/pion/logging v0.2.4
+	github.com/pion/stun/v3 v3.1.1
 	github.com/pion/transport/v2 v2.2.10
+	github.com/pion/transport/v4 v4.0.1
+	github.com/pion/webrtc/v4 v4.2.11
 	github.com/pires/go-proxyproto v0.11.0
 	github.com/quic-go/quic-go v0.59.0
 	github.com/refraction-networking/utls v1.8.2
@@ -54,28 +59,35 @@ require (
 	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
 	github.com/ebfe/bcrypt_pbkdf v0.0.0-20140212075826-3c8d2dcb253a // indirect
 	github.com/google/btree v1.1.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jhump/protoreflect/v2 v2.0.0-beta.1 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/klauspost/reedsolomon v1.11.7 // indirect
 	github.com/mustafaturan/monoton v1.0.0 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	github.com/pion/dtls/v2 v2.0.0-rc.7 // indirect
-	github.com/pion/logging v0.2.4 // indirect
+	github.com/pion/datachannel v1.6.0 // indirect
+	github.com/pion/dtls/v2 v2.2.12 // indirect
+	github.com/pion/interceptor v0.1.44 // indirect
+	github.com/pion/mdns/v2 v2.1.0 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/sctp v1.8.7 // indirect
-	github.com/pion/transport/v4 v4.0.1 // indirect
+	github.com/pion/rtcp v1.2.16 // indirect
+	github.com/pion/rtp v1.10.1 // indirect
+	github.com/pion/sctp v1.9.4 // indirect
+	github.com/pion/sdp/v3 v3.0.18 // indirect
+	github.com/pion/srtp/v3 v3.0.10 // indirect
+	github.com/pion/turn/v4 v4.1.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/quic-go/qpack v0.6.0 // indirect
 	github.com/riobard/go-bloom v0.0.0-20200614022211-cdc8013cb5b3 // indirect
 	github.com/secure-io/siv-go v0.0.0-20180922214919-5ff40651e2c4 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/wlynxg/anet v0.0.5 // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/mod v0.33.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
 	golang.org/x/time v0.12.0 // indirect
 	golang.org/x/tools v0.42.0 // indirect
-	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
