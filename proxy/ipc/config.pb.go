5db9da2190f0b91b3cfd23660fb84774b1632a85
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-04-23 02:12:02 +0800

    Add an IPC inbound protocol for IPC purposes

---
+++ proxy/ipc/config.pb.go
@@ -0,0 +1,119 @@
+package ipc
+
+import (
+	_ "github.com/v2fly/v2ray-core/v5/common/protoext"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+type ServerConfig struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Level         int32                  `protobuf:"varint,1,opt,name=level,proto3" json:"level,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *ServerConfig) Reset() {
+	*x = ServerConfig{}
+	mi := &file_proxy_ipc_config_proto_msgTypes[0]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ServerConfig) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ServerConfig) ProtoMessage() {}
+
+func (x *ServerConfig) ProtoReflect() protoreflect.Message {
+	mi := &file_proxy_ipc_config_proto_msgTypes[0]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ServerConfig.ProtoReflect.Descriptor instead.
+func (*ServerConfig) Descriptor() ([]byte, []int) {
+	return file_proxy_ipc_config_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *ServerConfig) GetLevel() int32 {
+	if x != nil {
+		return x.Level
+	}
+	return 0
+}
+
+var File_proxy_ipc_config_proto protoreflect.FileDescriptor
+
+const file_proxy_ipc_config_proto_rawDesc = "" +
+	"\n" +
+	"\x16proxy/ipc/config.proto\x12\x14v2ray.core.proxy.ipc\x1a common/protoext/extensions.proto\"8\n" +
+	"\fServerConfig\x12\x14\n" +
+	"\x05level\x18\x01 \x01(\x05R\x05level:\x12\x82\xb5\x18\x0e\n" +
+	"\ainbound\x12\x03ipcB]\n" +
+	"\x18com.v2ray.core.proxy.ipcP\x01Z(github.com/v2fly/v2ray-core/v5/proxy/ipc\xaa\x02\x14V2Ray.Core.Proxy.IPCb\x06proto3"
+
+var (
+	file_proxy_ipc_config_proto_rawDescOnce sync.Once
+	file_proxy_ipc_config_proto_rawDescData []byte
+)
+
+func file_proxy_ipc_config_proto_rawDescGZIP() []byte {
+	file_proxy_ipc_config_proto_rawDescOnce.Do(func() {
+		file_proxy_ipc_config_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_proxy_ipc_config_proto_rawDesc), len(file_proxy_ipc_config_proto_rawDesc)))
+	})
+	return file_proxy_ipc_config_proto_rawDescData
+}
+
+var file_proxy_ipc_config_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_proxy_ipc_config_proto_goTypes = []any{
+	(*ServerConfig)(nil), // 0: v2ray.core.proxy.ipc.ServerConfig
+}
+var file_proxy_ipc_config_proto_depIdxs = []int32{
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
+}
+
+func init() { file_proxy_ipc_config_proto_init() }
+func file_proxy_ipc_config_proto_init() {
+	if File_proxy_ipc_config_proto != nil {
+		return
+	}
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_proxy_ipc_config_proto_rawDesc), len(file_proxy_ipc_config_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   1,
+			NumExtensions: 0,
+			NumServices:   0,
+		},
+		GoTypes:           file_proxy_ipc_config_proto_goTypes,
+		DependencyIndexes: file_proxy_ipc_config_proto_depIdxs,
+		MessageInfos:      file_proxy_ipc_config_proto_msgTypes,
+	}.Build()
+	File_proxy_ipc_config_proto = out.File
+	file_proxy_ipc_config_proto_goTypes = nil
+	file_proxy_ipc_config_proto_depIdxs = nil
+}
